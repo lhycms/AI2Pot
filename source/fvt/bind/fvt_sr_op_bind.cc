2cb84d9370976bd50a9d91106c26d8e373f089de
lhycms <1404293933@qq.com>
2024-05-05 22:40:37 +0800

    Finish DeepMD with custom operator

--- source/fvt/bind/fvt_sr_op_bind.cc
+++ source/fvt/bind/fvt_sr_op_bind.cc
@@ -4,6 +4,7 @@
 #include <cassert>
 
 #include "../include/force_sr_op.h"
+#include "../include/virial_sr_op.h"
 
 TORCH_LIBRARY(fvt, m) {
     m.def(
@@ -21,6 +22,21 @@ TORCH_LIBRARY(fvt, m) {
                                             bnumneigh_tensor,
                                             bfirstneigh_tensor,
                                             (int)nghost,
+                                            (int)umax_num_neighs,
+                                            bdei_drij_tensor);
+        }
+    );
+
+    m.def(
+        "VirialSrOp",
+        [](const at::Tensor &bnumneigh_tensor,
+           const at::Tensor &brcs_tensor,
+           int64_t umax_num_neighs,
+           const at::Tensor &bdei_drij_tensor)
+        {
+            assert(umax_num_neighs <= INT_MAX);
+            return ai2pot::fvt::VirialSrOp(bnumneigh_tensor,
+                                            brcs_tensor,
                                             (int)umax_num_neighs,
                                             bdei_drij_tensor);
         }
