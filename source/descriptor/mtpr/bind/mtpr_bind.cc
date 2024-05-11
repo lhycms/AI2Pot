e2831704d8b36c5f58644dac0535163c9d38fd5d
lhycms <1404293933@qq.com>
2024-05-12 00:03:24 +0800

    Finish mtpBasisOp

--- source/descriptor/mtpr/bind/mtpr_bind.cc
+++ source/descriptor/mtpr/bind/mtpr_bind.cc
@@ -4,6 +4,7 @@
 #include <cassert>
 
 #include "../include/mtpParamOp.h"
+#include "../include/mtpBasisOp.h"
 
 
 TORCH_LIBRARY(mtpr, m) {
@@ -16,5 +17,48 @@ TORCH_LIBRARY(mtpr, m) {
         }
     );
 
-    
+    m.def(
+        "mtpBasisOp",
+        [](const at::Tensor &alpha_index_basic_tensor,
+           const at::Tensor &alpha_index_times_tensor,
+           const at::Tensor &alpha_moment_mapping_tensor,
+           const at::Tensor &num_mus4moms_tensor,
+           const at::Tensor &mus4moms_tensor,
+           const at::Tensor &nmus_tensor,
+           int64_t ntypes,
+           int64_t chebyshev_size,
+           const at::Tensor &coeffs_tensor,
+           const at::Tensor &bilist_tensor,
+           const at::Tensor &bnumneigh_tensor,
+           const at::Tensor &bfirstneigh_tensor,
+           const at::Tensor &brcs_tensor,
+           const at::Tensor &btypes_tensor,
+           int64_t umax_num_neighs,
+           double rmax,
+           double rmin)
+        {
+            assert(ntypes <= INT_MAX);
+            assert(chebyshev_size <= INT_MAX);
+            assert(umax_num_neighs <= INT_MAX);
+
+            return ai2pot::mtpr::MtpBasisOp(
+                alpha_index_basic_tensor,
+                alpha_index_times_tensor,
+                alpha_moment_mapping_tensor,
+                num_mus4moms_tensor,
+                mus4moms_tensor,
+                nmus_tensor,
+                (int)ntypes,
+                (int)chebyshev_size,
+                coeffs_tensor,
+                bilist_tensor,
+                bnumneigh_tensor,
+                bfirstneigh_tensor,
+                brcs_tensor,
+                btypes_tensor,
+                (int)umax_num_neighs,
+                rmax,
+                rmin);
+        }
+    );
 }
