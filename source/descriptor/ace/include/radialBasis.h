ccd2a7acbf2427f7a5221f16a555e92ba2ca5dbc
lhycms <1404293933@qq.com>
2024-06-19 21:40:13 +0800

    ACE radialBasis

--- source/descriptor/ace/include/radialBasis.h
+++ source/descriptor/ace/include/radialBasis.h
@@ -2,6 +2,7 @@
 #define AI2POT_ACE_RADIAL_BASIS_H
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
 #include <cassert>
 
@@ -27,6 +28,55 @@ private:
     CoordType _rmax = 0;
     CoordType _rmin = 0;
 };  // class : SwitchFunction
+
+
+template <typename CoordType>
+class RB_CChebyshev {
+public:
+    RB_CChebyshev();
+
+    RB_CChebyshev(int size,
+                  CoordType rmax,
+                  CoordType rmin,
+                  CoordType lambda_val);
+
+    //RB_CChebyshev(const RB_CChebyshev &rhs);
+
+    //RB_CChebyshev(RB_CChebyshev &&rhs);
+
+    //RB_CChebyshev& operator=(const RB_CChebyshev &rhs);
+
+    //RB_CChebyshev& operator=(RB_CChebyshev &&rhs);
+
+    void build(CoordType distance_ij);
+
+    ~RB_CChebyshev();
+
+    const int size() const;
+
+    const CoordType rmax() const;
+
+    const CoordType rmin() const;
+
+    const CoordType lambda_val() const;
+
+    const CoordType* vals() const;
+
+    const CoordType* ders2vv() const;
+
+    const CoordType* ders2r() const;
+
+private:
+    int _size = 0;
+    CoordType _rmax = 0.0;
+    CoordType _rmin = 0.0;
+    CoordType _lambda_val = 0.0;
+    CoordType *_vals = nullptr;
+    CoordType *_ders2vv = nullptr;
+    CoordType *_ders2r = nullptr;
+};  // class : RB_CChebyshev
+
+
 
 
 
@@ -80,6 +130,107 @@ CoordType SwitchFunction<CoordType>::rmax() const {
     return this->_rmax;
 }
 
+
+template <typename CoordType>
+RB_CChebyshev<CoordType>::RB_CChebyshev() {
+    this->_size = 0;
+    this->_rmin = 0.0;
+    this->_rmax = 0.0;
+    this->_lambda_val = 0.0;
+    this->_vals = nullptr;
+    this->_ders2vv = nullptr;
+    this->_ders2r = nullptr;
+}
+
+template <typename CoordType>
+RB_CChebyshev<CoordType>::RB_CChebyshev(int size,
+                                        CoordType rmax,
+                                        CoordType rmin,
+                                        CoordType lambda_val)
+{
+    this->_size = size;
+    this->_rmin = rmin;
+    this->_rmax = rmax;
+    this->_lambda_val = lambda_val;
+    this->_vals = (CoordType*)malloc(sizeof(CoordType) * this->_size);
+    memset(this->_vals, 0, this->_size * sizeof(CoordType));
+    this->_ders2vv = (CoordType*)malloc(sizeof(CoordType) * this->_size);
+    memset(this->_ders2vv, 0, this->_size * sizeof(CoordType));
+    this->_ders2r = (CoordType*)malloc(sizeof(CoordType) * this->_size);
+    memset(this->_ders2r, 0, this->_size * sizeof(CoordType));
+}
+
+template <typename CoordType>
+void RB_CChebyshev<CoordType>::build(CoordType distance_ij)
+{
+    CoordType uu = (distance_ij - this->_rmin) / (this->_rmax - this->_rmin);
+    CoordType vv = 1 - 2 * ( exp( -this->_lambda_val*(uu-1) ) - 1 ) / (exp(this->_lambda_val) - 1);
+
+    for (int ii=0; ii<this->_size; ii++) {
+        if (ii==0) {
+            this->_vals[ii] = 1;
+            this->_ders2vv[ii] = 0;
+            this->_ders2r[ii] = 0;
+        } else if (ii==1) {
+            this->_vals[ii] = vv;
+            this->_ders2vv[ii] = 1;
+            this->_ders2r[ii] = this->_ders2vv[ii]
+                                * 2*this->_lambda_val/(this->_rmax-this->_rmin) 
+                                * exp(-this->_lambda_val*((distance_ij-this->_rmin)/(this->_rmax-this->_rmin)-1)) 
+                                / (exp(this->_lambda_val)-1);
+        } else {
+            this->_vals[ii] = 2 * vv * this->_vals[ii-1] - this->_vals[ii-2];
+            this->_ders2vv[ii] = 2*this->_vals[ii-1] + 2*vv*this->_ders2vv[ii-1] - this->_ders2vv[ii-2];
+            this->_ders2r[ii] = this->_ders2vv[ii]
+                                * 2*this->_lambda_val/(this->_rmax-this->_rmin) 
+                                * exp(-this->_lambda_val*((distance_ij-this->_rmin)/(this->_rmax-this->_rmin)-1)) 
+                                / (exp(this->_lambda_val)-1);
+        }
+    }
+}
+
+
+template <typename CoordType>
+RB_CChebyshev<CoordType>::~RB_CChebyshev() {
+    free(this->_vals);
+    free(this->_ders2vv);
+    free(this->_ders2r);
+}
+
+template <typename CoordType>
+const int RB_CChebyshev<CoordType>::size() const {
+    return this->_size;
+}
+
+template <typename CoordType>
+const CoordType RB_CChebyshev<CoordType>::rmax() const {
+    return this->_rmax;
+}
+
+template <typename CoordType>
+const CoordType RB_CChebyshev<CoordType>::rmin() const {
+    return this->_rmin;
+}
+
+template <typename CoordType>
+const CoordType RB_CChebyshev<CoordType>::lambda_val() const {
+    return this->_lambda_val;
+}
+
+template <typename CoordType>
+const CoordType* RB_CChebyshev<CoordType>::vals() const {
+    return this->_vals;
+}
+
+template <typename CoordType>
+const CoordType* RB_CChebyshev<CoordType>::ders2vv() const {
+    return this->_ders2vv;
+}
+
+template <typename CoordType>
+const CoordType* RB_CChebyshev<CoordType>::ders2r() const {
+    return this->_ders2r;
+}
 
 };  // namespace : ace
 };  // namespace : ai2pot
--- source/descriptor/ace/tests/test_radialBasis.cc
+++ source/descriptor/ace/tests/test_radialBasis.cc
@@ -31,6 +31,36 @@ protected:
 };  // class : SwitchFunction
 
 
+class RB_CChebyshevTest: public ::testing::Test
+{
+protected:
+    int size;
+    double rmax;
+    double rmin;
+    double lambda_val;
+    double distance_ij;
+
+    static void SetUpTestSuite() {
+        std::cout << "RB_CChebyshevTest (TestSuite) is setting up...\n";
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "RB_CChebyshevTest (TestSuite) is tearing down...\n";
+    }
+
+    void SetUp() override {
+        size = 8;
+        rmax = 5.0;
+        rmin = 2.0;
+        lambda_val = 5;
+        distance_ij = 3.14;
+    }
+
+    void TearDown() override {
+    }
+};  // class : RB_CChebyshevTest
+
+
 TEST_F(SwitchFunctionTest, default_constructor) {
     ai2pot::ace::SwitchFunction<double> switch_func;
 
@@ -58,6 +88,46 @@ TEST_F(SwitchFunctionTest, der2r_accuracy) {
     double deriv2r_val = ( switch_func.val(distance_ij+0.0001) - switch_func.val(distance_ij) ) / 0.0001;
     printf("\t1. Numerical derivative: %lf\n", deriv2r_val);
     printf("\t2. Analytical derivative: %lf\n", switch_func.der2r(distance_ij));
+}
+
+
+TEST_F(RB_CChebyshevTest, default_constructor) {
+    ai2pot::ace::RB_CChebyshev<double> rb_cchebyshev;
+    ASSERT_EQ(rb_cchebyshev.rmax(), 0.0);
+    ASSERT_EQ(rb_cchebyshev.rmin(), 0.0);
+    ASSERT_EQ(rb_cchebyshev.size(), 0);
+    ASSERT_EQ(rb_cchebyshev.lambda_val(), 0.0);
+}
+
+TEST_F(RB_CChebyshevTest, ders2r_accuracy) {
+    ai2pot::ace::RB_CChebyshev<double> rb_cchebyshev(size, rmax, rmin, lambda_val);
+    rb_cchebyshev.build(distance_ij);
+    ai2pot::ace::RB_CChebyshev<double> delta_rb_cchebyshev(size, rmax, rmin, lambda_val);
+    delta_rb_cchebyshev.build(distance_ij+0.001);
+
+printf("1.1. Values of rb_cchebyshev:\n\t");
+for (int ii=0; ii<rb_cchebyshev.size(); ii++) {
+    printf("%lf, ", rb_cchebyshev.vals()[ii]);
+}
+printf("\n");
+
+printf("1.2. Values of delta_rb_cchebyshev:\n\t");
+for (int ii=0; ii<delta_rb_cchebyshev.size(); ii++) {
+    printf("%lf, ", delta_rb_cchebyshev.vals()[ii]);
+}
+printf("\n");
+
+printf("2.1. Analytic ders2r:\n\t");
+for (int ii=0; ii<rb_cchebyshev.size(); ii++) {
+    printf("%lf, ", rb_cchebyshev.ders2r()[ii]);
+}
+printf("\n");
+
+printf("2.2. Numerical ders2r:\n\t");
+for (int ii=0; ii<rb_cchebyshev.size(); ii++) {
+    printf("%lf, ", (delta_rb_cchebyshev.vals()[ii] - rb_cchebyshev.vals()[ii]) / 0.001);
+}
+printf("\n");
 }
 
 
