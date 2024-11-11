96f59ff520667f9657f003deb28e9a6c7f005d93
lhycms <1404293933@qq.com>
2024-11-11 14:45:08 +0800

    MolecularDynamics

--- ai2pot/chgnet/__init__.py
+++ ai2pot/chgnet/__init__.py
@@ -3,3 +3,4 @@ from chgnet.utils import read_json
 from chgnet.data.dataset import StructureData, get_train_val_test_loader
 from chgnet.model import CHGNet
 from chgnet.trainer import trainer
+from chgnet.model.dynamics import MolecularDynamics
