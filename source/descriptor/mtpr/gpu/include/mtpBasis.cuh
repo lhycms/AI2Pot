09db1a9ed8aba6d8d2d62e9150f378a4be0d9b61
lhycms <1404293933@qq.com>
2024-11-14 22:10:06 +0800

    Finish find_mtp_basis_val_der_cuda_kernel()

--- source/descriptor/mtpr/gpu/include/mtpBasis.cuh
+++ source/descriptor/mtpr/gpu/include/mtpBasis.cuh
@@ -9,6 +9,9 @@
 #include "./mtp_utilities.cuh"
 
 
+namespace ai2pot {
+namespace mtpr {
+
 template <typename CoordType>
 static __host__ __device__ 
 void find_switch_func(CoordType &val,
@@ -68,6 +71,35 @@ void find_mtp_basis_val_der_cuda_kernel(
     double rmax,
     double rmin);
 
+
+template <typename CoordType>
+void find_mtp_basis_val_der_cuda_launcher(
+    CoordType *mtp_basis_val,
+    CoordType (*mtp_basis_der)[3],
+    CoordType *mtp_basis_der2coeffs,
+    int chebyshev_size,
+    CoordType *coeffs,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    const int max_num_mus4mom,
+    const int *num_mus4moms,
+    const int *mus4moms_ptr,
+    int nmus,
+    int inum,
+    int *ilist,
+    int *numneigh,
+    int *firstneigh,
+    CoordType (*relative_coords)[3],
+    int *types,
+    int ntypes,
+    int umax_num_neigh_atoms,
+    double rmax,
+    double rmin);
 
 
 template <typename CoordType>
@@ -286,7 +318,8 @@ __global__ void find_mtp_basis_val_der_cuda_kernel(
             }
         }
 
-        for (int i=0; i<alpha_index_times_count; i++) {
+        for (int i=0; i<alpha_index_times_count; i++) 
+        {
             CoordType val0 = mom_vals[alpha_index_times[i][0]];
             CoordType val1 = mom_vals[alpha_index_times[i][1]];
             CoordType val2 = alpha_index_times[i][2];
@@ -296,42 +329,114 @@ __global__ void find_mtp_basis_val_der_cuda_kernel(
                 for (int tmp_type_outer=0; tmp_type_outer<ntypes; tmp_type_outer++) {
                     for (int q=0; q<num_mus4moms[alpha_index_times[i][0]]; q++) {
                         for (int xi=0; xi<chebyshev_size; xi++) {
-                            int idx0 = (tmp_type_centeral*ntypes + tmp_type_outer)*nmus*chebyshev_size
-                                       + mus4moms_ptr[alpha_index_times[i][0]*max_num_mus4mom + q]*chebyshev_size
-                                       + xi;
-                            mom_ders2coeffs[alpha_index_times[i][3]][tmp_type_centeral][tmp_type_outer][mus4moms_ptr[alpha_index_times[i][0]]*max_num_mus4mom + q][xi] += val2
-                                * mom_ders2coeffs[alpha_index_times[i][0]*num_coeffs + idx0]
+                            mom_ders2coeffs[alpha_index_times[i][3]][tmp_type_centeral][tmp_type_outer][mus4moms_ptr[alpha_index_times[i][0]*max_num_mus4mom + q]][xi] += val2
+                                * mom_ders2coeffs[alpha_index_times[i][0]][tmp_type_centeral][tmp_type_outer][mus4moms_ptr[alpha_index_times[i][0]*max_num_mus4mom + q]][xi]
                                 * val1;
                         }
                     }
                     for (int q=0; q<num_mus4moms[alpha_index_times[i][1]]; q++) {
                         for (int xi=0; xi<chebyshev_size; xi++) {
-                            int idx1 = (tmp_type_centeral*ntypes + tmp_type_outer)*nmus*chebyshev_size
-                                       + mus4moms_ptr[alpha_index_times[i][1]*max_num_mus4mom + q]*chebyshev_size
-                                       + xi;
-                            mom_ders2coeffs[alpha_index_times[i][3]*num_coeffs + idx1] += val2
+                            mom_ders2coeffs[alpha_index_times[i][3]][tmp_type_centeral][tmp_type_outer][mus4moms_ptr[alpha_index_times[i][1]*max_num_mus4mom + q]][xi] += val2
                                 * val0
-                                * mom_ders2coeffs[alpha_index_times[i][1]*num_coeffs + idx1];
+                                * mom_ders2coeffs[alpha_index_times[i][1]][tmp_type_centeral][tmp_type_outer][mus4moms_ptr[alpha_index_times[i][1]*max_num_mus4mom + q]][xi];
                         }
                     }
                 }
             }
 
-            for (int jj=0; jj<numneigh[ii]; jj++) {
-                
+            for (int jj=0; jj<numneigh[ii]; jj++) 
+            {
+                mom_ders[alpha_index_times[i][3]][jj][0] += val2 * 
+                    ( mom_ders[alpha_index_times[i][0]][jj][0] * val1
+                      + val0 * mom_ders[alpha_index_times[i][1]][jj][0] );
+                mom_ders[alpha_index_times[i][3]][jj][1] += val2 *
+                    ( mom_ders[alpha_index_times[i][0]][jj][1] * val1
+                      + val0 * mom_ders[alpha_index_times[i][1]][jj][1]);
+                mom_ders[alpha_index_times[i][3]][jj][2] += val2 *
+                    ( mom_ders[alpha_index_times[i][0]][jj][2] * val1
+                      + val0 * mom_ders[alpha_index_times[i][1]][jj][2]);
             }
-
         }
 
-
-
+        for (int i=0; i<alpha_scalar_moments; i++)
+        {
+            mtp_basis_val[ii*alpha_moments_count + i] = mom_vals[alpha_moment_mapping[i]];
+            for (int jj=0; jj<numneigh; jj++)
+                for (int a=0; a<3; a++)
+                    mtp_basis_der[ii*alpha_moments_count*umax_num_neigh_atoms
+                                  + alpha_moment_mapping[i]*umax_num_neigh_atoms
+                                  + jj][a] = mom_ders[alpha_moment_mapping[i]][jj][a];
+            for (int idx=0; idx<num_coeffs; idx++)
+                mtp_basis_der2coeffs[ii*alpha_scalar_moments*num_coeffs + i*num_coeffs + idx] = mom_ders2coeffs[alpha_moment_mapping[i]*num_coeffs + idx];
+        }
     }
 }
 
-//template <typename CoordType>
-//void find_mtp_basis_val_der_cuda_launcher()
-//{
-//}
+
+template <typename CoordType>
+void find_mtp_basis_val_der_cuda_launcher(CoordType *mtp_basis_val,
+                                          CoordType (*mtp_basis_der)[3],
+                                          CoordType *mtp_basis_der2coeffs,
+                                          int chebyshev_size,
+                                          CoordType *coeffs,
+                                          const int alpha_moments_count,
+                                          const int alpha_index_basic_count,
+                                          const int (*alpha_index_basic)[4],
+                                          const int alpha_index_times_count,
+                                          const int (*alpha_index_times)[4],
+                                          const int alpha_scalar_moments,
+                                          const int *alpha_moment_mapping,
+                                          const int max_num_mus4mom,
+                                          const int *num_mus4moms,
+                                          const int *mus4moms_ptr,
+                                          int nmus,
+                                          int inum,
+                                          int *ilist,
+                                          int *numneigh,
+                                          int *firstneigh,
+                                          CoordType (*relative_coords)[3],
+                                          int *types,
+                                          int ntypes,
+                                          int umax_num_neigh_atoms,
+                                          double rmax,
+                                          double rmin)
+{
+    const int block_size_x = 256;
+    const int grid_size_x = (inum - 1) / block_size_x + 1;
+    find_mtp_basis_val_der_cuda_kernel<CoordType> KERNEL_ARG2(grid_size_x, block_size_x) (
+        mtp_basis_val,
+        mtp_basis_der,
+        mtp_basis_der2coeffs,
+        chebyshev_size,
+        coeffs,
+        alpha_moments_count,
+        alpha_index_basic_count,
+        alpha_index_basic,
+        alpha_index_times_count,
+        alpha_index_times,
+        alpha_scalar_moments,
+        alpha_moment_mapping,
+        max_num_mus4mom,
+        num_mus4moms,
+        mus4moms_ptr,
+        nmus,
+        inum,
+        ilist,
+        numneigh,
+        firstneigh,
+        relative_coords,
+        types,
+        ntypes,
+        umax_num_neigh_atoms,
+        rmax,
+        rmin);
+    CHECK( cudaDeviceSynchronize() );
+}
+
+
+};  // namespace : mtpr
+};  // namespace : ai2pot
+
 
 
 #endif
