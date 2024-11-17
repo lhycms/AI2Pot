de89d6ef580878e057603b2460018a464aa87a7e
lhycms <1404293933@qq.com>
2024-11-17 22:50:33 +0800

    GPU: find_force_sr_val_der()

--- source/fvt/gpu/include/force_sr.cuh
+++ source/fvt/gpu/include/force_sr.cuh
@@ -19,6 +19,29 @@ void find_force_sr_val_der(
     int umax_num_neighs,
     CoordType *dei_drij);
 
+template <typename CoordType>
+__global__
+void find_force_sr_val(
+    CoordType *force_sr_val,
+    int inum,
+    int *ilist,
+    int *numneigh,
+    int *firstneigh,
+    int nghost,
+    int umax_num_neighs,
+    CoordType *dei_drij);
+
+template <typename CoordType>
+__global__
+void find_force_sr_der(
+    CoordType *force_sr_der,
+    int inum,
+    int *ilist,
+    int *numneigh,
+    int *firstneigh,
+    int nghost,
+    int umax_num_neighs,
+    CoordType *dei_drij);
 
 
 template <typename CoordType>
@@ -45,6 +68,62 @@ void find_force_sr_val_der(
             int tmp_idx = ii*umax_num_neighs*3 + jj*3 + kk;
             force_sr_val[center_idx*3 + kk] += dei_drij[tmp_idx];
             force_sr_val[neigh_idx*3 + kk] -= dei_drij[tmp_idx];
+            force_sr_der[(center_idx*3 + kk)*inum*umax_num_neighs*3 + tmp_idx] += 1;
+            force_sr_der[(neigh_idx*3 + kk)*inum*umax_num_neighs*3 + tmp_idx] -= 1;
+        }
+    }
+}
+
+
+template <typename CoordType>
+__global__
+void find_force_sr_val(
+    CoordType *force_sr_val,
+    int inum,
+    int *ilist,
+    int *numneigh,
+    int *firstneigh,
+    int nghost,
+    int umax_num_neighs,
+    CoordType *dei_drij)
+{
+    const int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int ny = blockIdx.y * blockDim.y + threadIdx.y;
+    if ((nx < inum) && (ny < umax_num_neighs)) {
+        int ii = nx;
+        int jj = ny;
+        int center_idx = ilist[ii];
+        int neigh_idx = firstneigh[ii*umax_num_neighs + jj];
+        for (int kk=0; kk<3; kk++) {
+            int tmp_idx = ii*umax_num_neighs*3 + jj*3 + kk;
+            force_sr_val[center_idx*3 + kk] += dei_drij[tmp_idx];
+            force_sr_val[neigh_idx*3 + kk] -= dei_drij[tmp_idx];
+        }
+    }
+}
+
+
+template <typename CoordType>
+__global__
+void find_force_sr_der(
+    CoordType *force_sr_der,
+    int inum,
+    int *ilist,
+    int *numneigh,
+    int *firstneigh,
+    int nghost,
+    int umax_num_neighs,
+    CoordType *dei_drij)
+{
+    const int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int ny = blockIdx.y * blockDim.y + threadIdx.y;
+    if ((nx < inum) && (ny < umax_num_neighs)) {
+        int ii = nx;
+        int jj = ny;
+        int center_idx = ilist[ii];
+        int neigh_idx = firstneigh[ii*umax_num_neighs + jj];
+        for (int kk=0; kk<3; kk++) {
+            int tmp_idx = ii*umax_num_neighs*3 + jj*3 + kk;
             force_sr_der[(center_idx*3 + kk)*inum*umax_num_neighs*3 + tmp_idx] += 1;
             force_sr_der[(neigh_idx*3 + kk)*inum*umax_num_neighs*3 + tmp_idx] -= 1;
         }
