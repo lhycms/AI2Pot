ea2ac5a34efe4245386d82e59708d0fb1767f48a
lhycms <1404293933@qq.com>
2024-12-19 23:28:15 +0800

    Finish test for DescriptorMtp

--- test/mtp/test_descriptor_mtp.py
+++ test/mtp/test_descriptor_mtp.py
@@ -224,11 +224,93 @@ class MtpDescriptorDerTest(unittest.TestCase):
     
     
     def test_etot_der2xyz(self):
-        pass
+        direction_idx_modify: int = 1
+        num_descriptor: int = self.descriptor_mtp.num_descriptors
+        linear: nn.Module = nn.Linear(num_descriptor, 1).to(self.dtype)
+        
+        nblist_info: List[torch.Tensor] = self.mlff_input.analyse_pymatgen(self.structure)
+        nblist_info[4].requires_grad_(True)
+        descriptor = self.descriptor_mtp(*nblist_info[1:-1])
+        etot = linear(descriptor)[0].sum()
+        etot.backward()
+        
+        nblist_info_: List[torch.Tensor] = [tensor.clone().detach() for tensor in nblist_info]
+        nblist_info_[4][0][self.center_idx_modify][self.neigh_idx_modify][direction_idx_modify] += self.delta
+        nblist_info_[4].requires_grad_(True)
+        descriptor_ = self.descriptor_mtp(*nblist_info_[1:-1])
+        etot_ = linear(descriptor_)[0].sum()
+        
+        print("---------------------------------------------------------------------------------------")
+        print("5.0. Etot = {0}".format(etot.item()))
+        print("5.1. Etot derivative w.r.t. rcs[{0}][{1}][{2}] calculated by custom code = {3}".format(
+                self.center_idx_modify,
+                self.neigh_idx_modify,
+                direction_idx_modify,
+                nblist_info[4].grad[0][self.center_idx_modify][self.neigh_idx_modify][direction_idx_modify].item())
+        )
+        print("5.2. Etot derivative w.r.t. rcs[{0}][{1}][{2}] calculated by finite difference method = {3}".format(
+                self.center_idx_modify,
+                self.neigh_idx_modify,
+                direction_idx_modify,
+                (etot_ - etot) / self.delta)
+        )
+        print("5.3. Difference between custom code and finite difference method = {0:.5f}".format(
+            nblist_info[4].grad[0][self.center_idx_modify][self.neigh_idx_modify][direction_idx_modify].item() \
+            - (etot_ - etot) / self.delta)
+        )
+        print("---------------------------------------------------------------------------------------")
+    
     
     
     def test_etot_der2coeffs(self):
-        pass
+        nblist_info: List[torch.Tensor] = self.mlff_input.analyse_pymatgen(self.structure)
+        itype_modify: int = nblist_info[5][0][self.center_idx_modify].item()
+        jtype_modify: int = nblist_info[5][0][self.neigh_idx_modify].item()
+        mu_modify: int = 0
+        xi_modify: int = 1
+        coeff_idx_modify: int = (itype_modify*self.ntypes + jtype_modify)*self.nmus*self.chebyshev_size + mu_modify*self.chebyshev_size + xi_modify
+        
+        num_descriptor: int = self.descriptor_mtp.num_descriptors
+        linear: nn.Module = nn.Linear(num_descriptor, 1).to(self.dtype)
+                
+        descriptor = self.descriptor_mtp(*nblist_info[1:-1])
+        etot = linear(descriptor)[0].sum()
+        etot.backward()
+        
+        coeffs_tensor_ = self.descriptor_mtp.coeffs.clone().detach()
+        coeffs_tensor_[coeff_idx_modify] += self.delta
+        coeffs_tensor_.requires_grad_(True)
+        self.descriptor_mtp_ = DescriptorMtp(mtp_level=self.mtp_level,
+                                             ntypes=self.ntypes,
+                                             chebyshev_size=self.chebyshev_size,
+                                             rmax=self.rmax,
+                                             rmin=self.rmin,
+                                             umax_num_neighs=self.umax_num_neigh).to(self.dtype)
+        self.descriptor_mtp_.register_parameter("coeffs", nn.Parameter(coeffs_tensor_, requires_grad=True))
+        descriptor_ = self.descriptor_mtp_(*nblist_info[1:-1])
+        etot_ = linear(descriptor_)[0].sum()
+        
+        print("---------------------------------------------------------------------------------------")
+        print("6.0. Etot = {0}".format(etot.item()))
+        print("6.1. Etot derivative w.r.t. coeffs[{0}][{1}][{2}][{3}] calculated by custom code = {4}".format(
+                itype_modify, 
+                jtype_modify, 
+                mu_modify, 
+                xi_modify,
+                self.descriptor_mtp.coeffs.grad[coeff_idx_modify].item())
+        )
+        print("6.2. Etot derivative w.r.t. coeffs[{0}][{1}][{2}][{3}] calculated by finite difference method = {4}".format(
+                itype_modify, 
+                jtype_modify, 
+                mu_modify, 
+                xi_modify,
+                (etot_ - etot) / self.delta)
+        )
+        print("6.3. Difference between custom code and finite difference method = {0:.5f}".format(
+            self.descriptor_mtp.coeffs.grad[coeff_idx_modify].item() \
+            - ((etot_ - etot) / self.delta).item())
+        )
+        print("---------------------------------------------------------------------------------------")
     
 
 if __name__ == "__main__":
