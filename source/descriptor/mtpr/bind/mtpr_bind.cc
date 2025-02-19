0a3ad4ac0f05cd621562b3235357b9d276547bdd
lhycms <1404293933@qq.com>
2025-02-19 17:44:02 +0800

    Encapsulate torch api

--- source/descriptor/mtpr/bind/mtpr_bind.cc
+++ source/descriptor/mtpr/bind/mtpr_bind.cc
@@ -20,6 +20,7 @@
 
 #include "../include/mtpParamOp.h"
 #include "../include/mtpBasisOp.h"
+#include "../include/linearMtpOp.h"
 
 
 TORCH_LIBRARY(mtpr, m) {
@@ -74,6 +75,69 @@ TORCH_LIBRARY(mtpr, m) {
                 brcs_tensor,
                 btypes_tensor,
                 (int)umax_num_neighs,
+                rmax,
+                rmin);
+        }
+    );
+
+    m.def(
+        "linearMtpToLossOp",
+        [](double e_weight,
+           double f_weight,
+           double v_weight,
+           const at::Tensor& betot_dft_tensor,
+           const at::Tensor& bforce_dft_tensor,
+           const at::Tensor& bvirial_dft_tensor,
+           int64_t chebyshev_size,
+           const at::Tensor& coeffs_tensor,
+           const at::Tensor& linear_coeffs_tensor,
+           const at::Tensor& type_bias_tensor,
+           int64_t alpha_moments_count,
+           const at::Tensor& alpha_index_basic_tensor,
+           const at::Tensor& alpha_index_times_tensor,
+           const at::Tensor& alpha_moment_mapping_tensor,
+           int64_t nmus,
+           const at::Tensor& binum_tensor,
+           const at::Tensor& bilist_tensor,
+           const at::Tensor& bnumneigh_tensor,
+           const at::Tensor& bfirstneigh_tensor,
+           const at::Tensor& brcs_tensor,
+           const at::Tensor& btypes_tensor,
+           int64_t ntypes,
+           int64_t nghost,
+           double rmax,
+           double rmin)
+        {   
+            assert(chebyshev_size < INT_MAX);
+            assert(alpha_moments_count < INT_MAX);
+            assert(nmus < INT_MAX);
+            assert(ntypes < INT_MAX);
+            assert(nghost < INT_MAX);
+
+            return ai2pot::mtpr::LinearMtpToLossOp(
+                e_weight,
+                f_weight,
+                v_weight,
+                betot_dft_tensor,
+                bforce_dft_tensor,
+                bvirial_dft_tensor,
+                (int)chebyshev_size,
+                coeffs_tensor,
+                linear_coeffs_tensor,
+                type_bias_tensor,
+                (int)alpha_moments_count,
+                alpha_index_basic_tensor,
+                alpha_index_times_tensor,
+                alpha_moment_mapping_tensor,
+                (int)nmus,
+                binum_tensor,
+                bilist_tensor,
+                bnumneigh_tensor,
+                bfirstneigh_tensor,
+                brcs_tensor,
+                btypes_tensor,
+                (int)ntypes,
+                (int)nghost,
                 rmax,
                 rmin);
         }
