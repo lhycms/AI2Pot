107a839358e5e7f9559ec5be599d78cdca85b4e8
lhycms <1404293933@qq.com>
2025-04-24 19:51:07 +0800

    Fix bug for calculate loss w.r.t. linear_coeffs

--- test/fromcc/test_linearMtp_accuracy.py
+++ test/fromcc/test_linearMtp_accuracy.py
@@ -71,9 +71,9 @@ class LinearMtpTest(unittest.TestCase):
     def test_linearMtpToLoss(self):
         # 1. Parameters
         e_weight: float = 1.0
-        f_weight: float = 0.0
+        f_weight: float = 1.0
         v_weight: float = 0.0
-        self.coeffs_tensor.requires_grad_(True)
+        self.coeffs_tensor.requires_grad_(False)
         self.linear_coeffs_tensor.requires_grad_(True)
         self.type_bias_tensor.requires_grad_(True)
         
