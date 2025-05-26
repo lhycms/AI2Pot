42d1afa4de454cb30bea658c6932b13f3481d441
lhycms <1404293933@qq.com>
2025-05-26 21:51:23 +0800

    Finish find_efv_kernel() function but not tested

--- source/descriptor/mtpr/gpu/include/linearMtp.cuh
+++ source/descriptor/mtpr/gpu/include/linearMtp.cuh
@@ -13,6 +13,40 @@ namespace mtpr {
 template <typename CoordType>
 static __host__ __device__
 void find_efv_atom(CoordType &etot,
+                   CoordType (*force)[3],
+                   CoordType *virial,
+                   int chebyshev_size,
+                   CoordType *coeffs,
+                   CoordType *linear_coeffs,
+                   CoordType *type_bias,
+                   const int alpha_moments_count,
+                   const int alpha_index_basic_count,
+                   const int (*alpha_index_basic)[4],
+                   const int alpha_index_times_count,
+                   const int (*alpha_index_times)[4],
+                   const int alpha_scalar_moments,
+                   const int *alpha_moment_mapping,
+                   int nmus,
+                   int silist,
+                   int snumneigh,
+                   int *sfirstneigh,
+                   CoordType (*srcs)[3],
+                   int *types,
+                   int ntypes,
+                   int *type_map,
+                   int umax_num_neigh_atoms,
+                   int nghost,
+                   CoordType rmax,
+                   CoordType rmin,
+                   CoordType zbl_rmax,
+                   CoordType zbl_rmin,
+                   CoordType *zbl_cks,
+                   CoordType *zbl_dks);
+
+
+template <typename CoordType>
+static __global__
+void find_efv_kernel(CoordType &etot,
                     CoordType (*force)[3],
                     CoordType *virial,
                     int chebyshev_size,
@@ -27,10 +61,11 @@ void find_efv_atom(CoordType &etot,
                     const int alpha_scalar_moments,
                     const int *alpha_moment_mapping,
                     int nmus,
-                    int silist,
-                    int snumneigh,
-                    int *sfirstneigh,
-                    CoordType (*srcs)[3],
+                    int inum,
+                    int *ilist,
+                    int *numneigh,
+                    int *firstneigh,
+                    int (*rcs)[3],
                     int *types,
                     int ntypes,
                     int *type_map,
@@ -42,6 +77,7 @@ void find_efv_atom(CoordType &etot,
                     CoordType zbl_rmin,
                     CoordType *zbl_cks,
                     CoordType *zbl_dks);
+
 
 
 
@@ -128,7 +164,7 @@ void find_efv_atom(CoordType &etot,
 
         auto_dist_powers_[0] = 1.0;
         for (int aa=0; aa<3; aa++)
-            auto_dist_powers_[0][aa] = NeighbVect[aa];
+            auto_dist_powers_[0][aa] = 1.0;
         for (int k=1; k<MAX_ALPHA_INDEX_BASIC; k++) {
             auto_dist_powers_[k] = auto_dist_powers_[k-1] * distance_ij;
             for (int aa=0; aa<3; aa++)
@@ -166,6 +202,7 @@ void find_efv_atom(CoordType &etot,
 
 
     // Step 3. Calculate EFV for atom
+    // Step 3.1. Energy
     CoordType e_site = 0;
     for (int i=0; i<alpha_scalar_moments; i++)
         e_site += linear_coeffs[i] * mom_vals[alpha_moment_mapping[i]];
@@ -185,8 +222,168 @@ void find_efv_atom(CoordType &etot,
                                                    * val2 * val1;
     }
 
+    // Step 3.2. Force and Virial.
     for (int jj=0; jj<snumneigh; jj++) {
-        
+        neigh_idx = sfirstneigh[jj];
+        type_outer = types[neigh_idx];
+        Zj = type_map[type_outer];
+        for (int aa=0; aa<3; aa++)
+            NeighbVect[aa] = srcs[jj][aa];
+        distance_ij = std::sqrt( std::pow(NeighbVect[0], 2)
+                                 + std::pow(NeighbVect[1], 2)
+                                 + std::pow(NeighbVect[2], 2) );
+        if (distance_ij > rmax)
+            continue;
+        distance_ij_inv = 1.0 / distance_ij;
+        CoordType rq_chebyshev_vals[MAX_CHEBYSHEV_SIZE] = {0.0};
+        CoordType rq_chebyshev_ders2r[MAX_CHEBYSHEV_SIZE] = {0.0};
+        find_rq_chebyshev<CoordType>(rq_chebyshev_vals,
+                                     rq_chebyshev_ders2r,
+                                     chebyshev_size,
+                                     rmax,
+                                     rmin,
+                                     distance_ij);
+        auto_dist_powers_[0] = 1.0;
+        for (int aa=0; aa<3; aa++)
+            auto_coords_powers_[0][aa] = 1.0;
+        for (int k=1; k<MAX_ALPHA_INDEX_BASIC; k++) {
+            auto_dist_powers_[k] = auto_dist_powers_[k-1] * distance_ij;
+            for (int aa=0; aa<3; aa++) 
+                auto_coords_powers_[k][aa] = auto_coords_powers_[k-1][aa] * NeighbVect[aa];
+        }
+
+        for (int i=0; i<alpha_index_basic_count; i++) 
+        {
+            int mu = alpha_index_basic[i][0];
+            int k = alpha_index_basic[i][1] + alpha_index_basic[i][2] + alpha_index_basic[i][3];
+            CoordType powk = 1.0 / auto_dist_powers_[k];
+            CoordType pow0 = auto_coords_powers_[alpha_index_basic[i][1]][0];
+            CoordType pow1 = auto_coords_powers_[alpha_index_basic[i][2]][1];
+            CoordType pow2 = auto_coords_powers_[alpha_index_basic[i][3]][2];
+            CoordType mult0 = pow0 * pow1 * pow2;
+            
+            for (int xi=0; xi<chebyshev_size; xi++) {
+                int idx = (type_central*ntypes + type_outer)*nmus*chebyshev_size + mu*chebyshev_size + xi;
+                CoordType A = rq_chebyshev_vals[xi];
+                CoordType B = mult0;
+                CoordType C = powk;
+                CoordType A_ders[3] = {0., 0., 0.};
+                CoordType B_ders[3] = {0., 0., 0.};
+                CoordType C_ders[3] = {0., 0., 0.};
+                A_ders[0] = rq_chebyshev_ders2r[xi] * NeighbVect[0] * distance_ij_inv;
+                A_ders[1] = rq_chebyshev_ders2r[xi] * NeighbVect[1] * distance_ij_inv;
+                A_ders[2] = rq_chebyshev_ders2r[xi] * NeighbVect[2] * distance_ij_inv;
+                if (alpha_index_basic[i][1] != 0) {
+                    B_ders[0] = alpha_index_basic[i][1]
+                                * auto_coords_powers_[alpha_index_basic[i][1] - 1][0]
+                                * pow1
+                                * pow2;
+                }
+                if (alpha_index_basic[i][2] != 0) {
+                    B_ders[1] = alpha_index_basic[i][2]
+                                * pow0
+                                * auto_coords_powers_[alpha_index_basic[i][2] - 1][1]
+                                * pow2;
+                }
+                if (alpha_index_basic[i][3] != 0) {
+                    B_ders[2] = alpha_index_basic[i][3]
+                                * pow0
+                                * pow1
+                                * auto_coords_powers_[alpha_index_basic[i][3] - 1][2];
+                }
+                C_ders[0] = -k * powk * distance_ij_inv * distance_ij_inv * NeighbVect[0];
+                C_ders[1] = -k * powk * distance_ij_inv * distance_ij_inv * NeighbVect[1];
+                C_ders[2] = -k * powk * distance_ij_inv * distance_ij_inv * NeighbVect[2];
+
+                for (int aa=0; aa<3; aa++) {
+                    int mom_der2xyz = coeffs[idx] * (A_ders[aa] * B * C
+                                                     + A * B_ders[aa] * C
+                                                     + A * B * C_ders[aa]);
+                    CoordType e_site_ders_ija = e_site_der2mom[i] * mom_der2xyz;
+                    atomicAdd(&force[center_idx][aa], e_site_ders_ija);
+                    atomicAdd(&force[neigh_idx][aa], -e_site_ders_ija);
+
+                    for (int bb=0; bb<3; bb++)
+                        atomicAdd(&virial[aa*3 + bb], -NeighbVect[aa] * e_site_ders_ija)
+                 }
+            }
+        }
+    }
+}
+
+
+template <typename CoordType>
+static __global__
+void find_efv_kernel(CoordType &etot,
+                    CoordType (*force)[3],
+                    CoordType *virial,
+                    int chebyshev_size,
+                    CoordType *coeffs,
+                    CoordType *linear_coeffs,
+                    CoordType *type_bias,
+                    const int alpha_moments_count,
+                    const int alpha_index_basic_count,
+                    const int (*alpha_index_basic)[4],
+                    const int alpha_index_times_count,
+                    const int (*alpha_index_times)[4],
+                    const int alpha_scalar_moments,
+                    const int *alpha_moment_mapping,
+                    int nmus,
+                    int inum,
+                    int *ilist,
+                    int *numneigh,
+                    int *firstneigh,
+                    int (*rcs)[3],
+                    int *types,
+                    int ntypes,
+                    int *type_map,
+                    int umax_num_neigh_atoms,
+                    int nghost,
+                    CoordType rmax,
+                    CoordType rmin,
+                    CoordType zbl_rmax,
+                    CoordType zbl_rmin,
+                    CoordType *zbl_cks,
+                    CoordType *zbl_dks)
+{
+    int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ii = nx;
+
+    if (ii < inum) {
+        int silist = ilist[ii];
+        int snumneigh = numneigh[ii];
+        int *sfirstneigh = &firstneigh[ii*umax_num_neigh_atoms];
+        CoordType (*srcs)[3] = (CoordType (*)[3])(&rcs[ii*umax_num_neigh_atoms][0]);
+        find_efv_atom<CoordType>(etot,
+                                 force,
+                                 virial,
+                                 chebyshev_size,
+                                 coeffs,
+                                 linear_coeffs,
+                                 type_bias,
+                                 alpha_moments_count,
+                                 alpha_index_basic_count,
+                                 alpha_index_basic,
+                                 alpha_index_times_count,
+                                 alpha_index_times,
+                                 alpha_scalar_moments,
+                                 alpha_moment_mapping,
+                                 nmus,
+                                 silist,
+                                 snumneigh,
+                                 sfirstneigh,
+                                 srcs,
+                                 types,
+                                 ntypes,
+                                 type_map,
+                                 umax_num_neigh_atoms,
+                                 nghost,
+                                 rmax,
+                                 rmin,
+                                 zbl_rmax,
+                                 zbl_rmin,
+                                 zbl_cks,
+                                 zbl_dks);
     }
 }
 
