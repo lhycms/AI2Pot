2142ea6aeec78803180f7c1be35951a2b9ec0a48
lhycms <1404293933@qq.com>
2025-06-13 10:53:57 +0800

    Fix bug: Enable CPU MTP parallel

--- test/models/mtp/test_linear_mtp.py
+++ test/models/mtp/test_linear_mtp.py
@@ -1,7 +1,9 @@
 import os
 import time
 import unittest
+from typing import List
 
+import numpy as np
 import torch
 from pymatgen.core import Structure
 from ai2pot.utils import (MlffInput, MlffToLossInput)
@@ -11,6 +13,9 @@ from ai2pot.models.mtp.linear_mtp import LinearMtp
 TEST_FILES_DIR = os.path.join(os.getenv("AI2POT_PATH"), "test", "test_data")
 ReNbSSe_POSCAR_PATH = os.path.join(TEST_FILES_DIR, "POSCARs", "POSCAR")
 torch.manual_seed(32)
+torch.set_num_threads(24)
+
+print(torch.get_num_threads())
 
 class LinearMtpTest(unittest.TestCase):
     def setUp(self):
@@ -20,7 +25,7 @@ class LinearMtpTest(unittest.TestCase):
         self.rmax: float = 5.0
         self.rmin: float = 2.0
         self.umax_num_neighs = 100
-        self.linear_mtp: LinearMtp = LinearMtp(mtp_level=16,
+        self.linear_mtp: LinearMtp = LinearMtp(mtp_level=20,
                                                type_map_tensor=self.type_map_tensor,
                                                chebyshev_size=self.chebyshev_size,
                                                rmax=self.rmax,
@@ -41,14 +46,21 @@ class LinearMtpTest(unittest.TestCase):
     
 
     def test_predict_loss(self):
-        t1 = time.time()
-        for _ in range(100):
+        times_list: List[float] = []
+        for ii in range(110):
+            t1 = time.time()
             loss = self.linear_mtp.predict_loss(*self.mlff_to_loss_input.analyse_pymatgen(self.structure,
                                                                                           e_weight=1.0,
                                                                                           f_weight=1.0,
                                                                                           v_weight=1.0))
-        t2 = time.time()
-        print("0. Average time cost by linear_mtp.predict_loss() = ", (t2 - t1) / 100)
+            loss.sum().backward()
+            t2 = time.time()
+
+            if (ii>9):
+                times_list.append(t2-t1)
+
+        print("0.1. Average time cost by linear_mtp.predict_loss() = ", np.sum(times_list) / 100)
+        print("0.2. std time cost by linear_mtp.predict_loss() = ", np.std(times_list) / 100)
         print("1. Loss = ", loss)
 
 
@@ -58,22 +70,32 @@ class LinearMtpTest(unittest.TestCase):
 
 
     def test_predict_efv(self):
-        t1 = time.time()
-        for _ in range(100):
+        times_list: List[float] = []
+        for ii in range(110):
+            t1 = time.time()
             e, f, v = self.linear_mtp.predict_efv(*self.mlff_input.analyse_pymatgen(structure=self.structure))
-        t2 = time.time()
-        print("0. Average time cost by linear_mtp.predict_efv() = ", (t2 - t1) / 100)
+            t2 = time.time()
+            if (ii>9):
+                times_list.append(t2-t1)
+
+        print("0.1. Average time cost by linear_mtp.predict_efv() = ", np.sum(times_list) / 100)
+        print("0.2. std time cost by linear_mtp.predict_efv() = ", np.std(times_list) / 100)
         print("1. Energy = ", e)
         print("2. Force.shape = ", f.shape)
         print("3. Virial.shape = ", v.shape)
 
     
     def test_predict_ef(self):
-        t1 = time.time()
-        for _ in range(100):
+        times_list: List[float] = []
+        for ii in range(110):
+            t1 = time.time()
             e, f = self.linear_mtp.predict_ef(*self.mlff_input.analyse_pymatgen(structure=self.structure))
-        t2 = time.time()
-        print("0. Average time cost by linear_mtp.predict_ef() = ", (t2 - t1) / 100)
+            t2 = time.time()
+            if (ii>9):
+                times_list.append(t2-t1)
+
+        print("0.1. Average time cost by linear_mtp.predict_ef() = ", np.sum(times_list) / 100)
+        print("0.2. std time cost by linear_mtp.predict_ef() = ", np.std(times_list) / 100)
         print("1. Energy = ", e)
         print("2. Force.shape = ", f.shape)
 
