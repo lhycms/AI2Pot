Fix std calculation ExtxyzDatasetPreprocess.get_dataset_target_statistics()

test/fromcc/test_linearMtp_accuracy.py
@@ -43,7 +43,7 @@ class LinearMtpTest(unittest.TestCase):
         self.umax_num_neigh_atoms: int = 200
         self.fit_virial: bool = False
         
-        """
+        
         self.ntypes: int = 4
         self.type_map: List[int] = [16, 34, 41, 75]
         self.type_map_tensor = torch.tensor(self.type_map, dtype=torch.int32).to(self.device)
@@ -60,6 +60,7 @@ class LinearMtpTest(unittest.TestCase):
                                                       [3.0, 0.0, 0]
                                                       ],
                                               coords_are_cartesian=True)
+        """
         print(self.structure)
     
         # 2. ZBL
@@ -107,7 +108,7 @@ class LinearMtpTest(unittest.TestCase):
         self.coeffs_tensor: torch.Tensor = torch.zeros(self.ntypes*self.ntypes*self.nmus*self.chebyshev_size, 
                                                        dtype=self.torch_float_dtype,
                                                        device=self.device)
-        nn.init.normal_(self.coeffs_tensor, mean=0.1, std=0.1)
+        nn.init.normal_(self.coeffs_tensor, mean=0.0, std=0.1)
         self.linear_coeffs_tensor: torch.Tensor = torch.zeros(self.alpha_moment_mapping_tensor.size(0),
                                                               dtype=self.torch_float_dtype,
                                                               device=self.device)
@@ -130,7 +131,7 @@ class LinearMtpTest(unittest.TestCase):
         print("LinearMtpTest (TestCase) is tearing down...\n")
     
     
-    def test_linearMtpToLoss(self):
+    def est_linearMtpToLoss(self):
         # 1. Parameters
         e_weight: float = 1.0
         f_weight: float = 0.1
@@ -229,7 +230,7 @@ class LinearMtpTest(unittest.TestCase):
         print("-------------------------------------------------")
 
 
-    def est_output_descriptors(self):
+    def test_output_descriptors(self):
         input_info: List[torch.Tensor] = self.mlff_input.analyse_pymatgen(self.structure)
         for item in input_info:
             item.to(self.device)
@@ -252,7 +253,7 @@ class LinearMtpTest(unittest.TestCase):
             input_info[6].item(),
             self.rmax,
             self.rmin)[0]
-        print("1. bdescriptor.shape = ", bdescriptors)
+        print("1. bdescriptor.shape = ", bdescriptors[0, 71, :])
 
 
 if __name__ == "__main__":

test/fromcc/test_targetStatisticsOp.py
@@ -28,7 +28,7 @@ class TargetStatisticsOpTest(unittest.TestCase):
         umax_num_neigh_atoms: int = 200
         pbc_xyz: List[bool] = [True, True, True]
         sort: bool = False
-        torch_float_dtype: torch._C.dtype = torch.float32
+        torch_float_dtype: torch._C.dtype = torch.float64
         has_virial: bool = False
         self.extxyz_dataset: ExtxyzDataset = ExtxyzDataset(filename=filename,
                                                       rcut=rcut,
@@ -56,15 +56,15 @@ class TargetStatisticsOpTest(unittest.TestCase):
             natoms_in_batch, ei_mean, ei_M2, force_M2 = results
 
             print("1. Calculate using torch:")
-            print("\t1.2. ei_mean = ", torch.sum(batch_data[7])/natoms_in_batch)
-            print("\t1.4. force_std = ", torch.std(batch_data[8]))
+            print("\t1.2. ei_mean = ", (torch.sum(batch_data[7])/natoms_in_batch).item())
+            print("\t1.4. force_std = ", torch.std(batch_data[8]).item())
             print("\n")
 
         print("2. Calculate using targetStatisticsOp:")
         print("\t2.1. natoms_in_batch = ", natoms_in_batch)
-        print("\t2.2. ei_mean = ", ei_mean)
-        print("\t2.3. ei_std = ", torch.sqrt(ei_M2/batch_size))
-        print("\t2.4. force_std = ", torch.sqrt(force_M2/(3*natoms_in_batch)))
+        print("\t2.2. ei_mean = ", ei_mean.item())
+        print("\t2.3. ei_std = ", torch.sqrt(ei_M2/(batch_size-1)).item())
+        print("\t2.4. force_std = ", torch.sqrt(force_M2/(3*natoms_in_batch-1)).item())
 
 
 if __name__ == "__main__":

test/utils/test_ExtxyzDatasetPreprocessor.py
@@ -26,7 +26,7 @@ class ExtxyzDatasetPreprocessorTest(unittest.TestCase):
         umax_num_neigh_atoms: int = 200
         pbc_xyz: List[bool] = [True, True, True]
         sort: bool = False
-        torch_float_dtype: torch._C.dtype = torch.float32
+        torch_float_dtype: torch._C.dtype = torch.float64
         has_virial: bool = False
         self.extxyz_dataset: ExtxyzDataset = ExtxyzDataset(filename=filename,
                                                       rcut=rcut,