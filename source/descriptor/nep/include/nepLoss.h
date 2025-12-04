d42a30475e7fbd8c52f5a6248a37527e4946d334
lhycms <1404293933@qq.com>
2025-12-04 18:51:26 +0800

    NepLoss find_ef_loss() && find_efv_loss()

--- source/descriptor/nep/include/nepLoss.h
+++ source/descriptor/nep/include/nepLoss.h
@@ -12,3 +12,127 @@
     You should have received a copy of the GNU General Public License
     along with AI2Pot.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#ifndef AI2POT_NEP_NEP_LOSS_H
+#define AI2POT_NEP_NEP_LOSS_H
+
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "./basis.h"
+#include "./nep_utilities.h"
+
+
+namespace ai2pot {
+namespace nep {
+
+
+template <typename CoordType>
+class NepLoss {
+public:
+    static void find_ef_loss(
+        CoordType &loss,
+        int inum,
+        int *ilist,
+        CoordType e_weight,
+        CoordType f_weight,
+        CoordType etot_ml,
+        CoordType etot_dft,
+        CoordType (*force_ml)[3],
+        CoordType (*force_dft)[3]);
+
+    static void find_loss(
+        CoordType &loss,
+        int inum,
+        int *ilist,
+        CoordType e_weight,
+        CoordType f_weight,
+        CoordType v_weight,
+        CoordType etot_ml,
+        CoordType etot_dft,
+        CoordType (*force_ml)[3],
+        CoordType (*force_dft)[3],
+        CoordType *virial_ml,
+        CoordType *virial_dft);
+};  // namespace : NepLoss
+
+
+template <typename CoordType>
+void NepLoss<CoordType>::find_ef_loss(
+    CoordType &loss,
+    int inum,
+    int *ilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType etot_ml,
+    CoordType etot_dft,
+    CoordType (*force_ml)[3],
+    CoordType (*force_dft)[3])
+{
+    loss = 0.0;
+
+    // Force term
+    CoordType f_loss = 0.0;
+    for (int ii=0; ii<inum; ii++) {
+        int center_idx = ilist[ii];
+        for (int aa=0; aa<3; aa++) {
+            f_loss += std::pow(force_ml[center_idx][aa] - force_dft[center_idx][aa], 2);
+        }
+    }
+    f_loss = f_weight / (3*inum) * f_loss;
+
+    // Energy term
+    CoordType e_loss = 0.0;
+    e_loss = e_weight / inum * std::pow(etot_ml - etot_dft, 2);
+
+    loss = f_loss + e_loss;
+}
+
+
+template <typename CoordType>
+void NepLoss<CoordType>::find_loss(
+    CoordType &loss,
+    int inum,
+    int *ilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType v_weight,
+    CoordType etot_ml,
+    CoordType etot_dft,
+    CoordType (*force_ml)[3],
+    CoordType (*force_dft)[3],
+    CoordType *virial_ml,
+    CoordType *virial_dft)
+{
+    loss = 0.0;
+
+    // Force term
+    CoordType f_loss = 0.0;
+    for (int ii=0; ii<inum; ii++) {
+        int center_idx = ilist[ii];
+        for (int aa=0; aa<3; aa++) {
+            f_loss += std::pow(force_ml[center_idx][aa] - force_dft[center_idx][aa], 2);
+        }
+    }
+    f_loss = f_weight / (3*inum) * f_loss;
+
+    // Virial term
+    CoordType v_loss = 0.0;
+    for (int aa=0; aa<3; aa++)
+        for (int bb=0; bb<3; bb++)
+            v_loss += std::pow(virial_ml[aa*3+bb] - virial_dft[aa*3+bb], 2);
+    v_loss = v_weight / (9*inum) * v_loss;
+
+    // Energy term
+    CoordType e_loss = 0;
+    e_loss = e_weight / inum * std::pow(etot_ml - etot_dft, 2);
+
+    loss = f_loss + v_loss + e_loss;
+}
+
+};  // namespace: nep
+};  // namespace : ai2pot
+
+
+#endif
