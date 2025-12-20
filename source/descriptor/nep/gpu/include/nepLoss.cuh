b940c2d023ee8ca936cd8196a2cb8c8b902ff59a
lhycms <1404293933@qq.com>
2025-12-21 01:04:25 +0800

    GPU nep find_loss_launcher() && find_ef_loss_launcher()

--- source/descriptor/nep/gpu/include/nepLoss.cuh
+++ source/descriptor/nep/gpu/include/nepLoss.cuh
@@ -16,6 +16,355 @@
 #ifndef AI2POT_NEP_NEP_LOSS_CUH
 #define AI2POT_NEP_NEP_LOSS_CUH
 
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdio.h>
+#include <math.h>
+#include <chrono>
+#include <iostream>
 
+#include "./nep_utilities.cuh"
+
+
+namespace ai2pot {
+namespace nep {
+
+template <typename CoordType>
+static __global__ 
+void find_loss_kernel(
+    CoordType *bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType v_weight,
+    CoordType *betot_ml,
+    CoordType *betot_dft,
+    CoordType (*bforce_ml)[3],
+    CoordType (*bforce_dft)[3],
+    CoordType *bvirial_ml,
+    CoordType *bvirial_dft);
+
+
+template <typename CoordType>
+static __host__
+void find_loss_launcher(
+    CoordType *h_bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *h_binum,
+    int *h_bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType v_weight,
+    CoordType *h_betot_ml,
+    CoordType *h_betot_dft,
+    CoordType (*h_bforce_ml)[3],
+    CoordType (*h_bforce_dft)[3],
+    CoordType *h_bvirial_ml,
+    CoordType *h_bvirial_dft);
+
+
+template <typename CoordType>
+static __global__
+void find_ef_loss_kernel(
+    CoordType &bloss,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType *betot_ml,
+    CoordType *betot_dft,
+    CoordType (*bforce_ml)[3],
+    CoordType (*bforce_dft)[3]);
+
+
+template <typename CoordType>
+static __host__
+void find_ef_loss_launcher(
+    CoordType *h_bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *h_binum,
+    int *h_bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType *h_betot_ml,
+    CoordType *h_betot_dft,
+    CoordType (*h_force_ml)[3],
+    CoordType (*h_force_dft)[3]);
+
+
+
+template <typename CoordType>
+__global__
+void find_loss_kernel(
+    CoordType *bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType v_weight,
+    CoordType *betot_ml,
+    CoordType *betot_dft,
+    CoordType (*bforce_ml)[3],
+    CoordType (*bforce_dft)[3],
+    CoordType *bvirial_ml,
+    CoordType *bvirial_dft)
+{
+    int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    int istruct = nx / natoms_pad;
+    if (istruct >= batch_size)
+        return;
+    int ii = nx % natoms_pad;
+    
+    CoordType *loss_ptr = &bloss_ptr[istruct];
+    CoordType etot_ml = betot_ml[istruct];
+    CoordType etot_dft = betot_dft[istruct];
+    CoordType (*force_ml)[3] = &bforce_ml[istruct*natoms_pad + 0];
+    CoordType (*force_dft)[3] = &bforce_dft[istruct*natoms_pad + 0];
+    CoordType *virial_ml = &bvirial_ml[istruct*9 + 0];
+    CoordType *virial_dft = &bvirial_dft[istruct*9 + 0];
+    int inum = binum[istruct];
+    int *ilist = &bilist[istruct*natoms_pad];
+
+    CoordType f_loss = 0.0;
+    if (ii < inum) {
+        int center_idx = ilist[ii];
+        for (int aa=0; aa<3; aa++)
+            f_loss += std::pow(force_ml[center_idx][aa] - force_dft[center_idx][aa], 2);
+        f_loss = f_weight / (3*inum) * f_loss;
+        atomicAdd(loss_ptr, f_loss);
+    }
+
+    if (nx == 0) {
+        CoordType v_loss = 0.0;
+        for (int aa=0; aa<3; aa++)
+            for (int bb=0; bb<3; bb++)
+                v_loss += std::pow(virial_ml[aa*3+bb] - virial_dft[aa*3+bb], 2);
+        v_loss = v_weight / (9*inum) * v_loss;
+        atomicAdd(loss_ptr, v_loss);
+
+        CoordType e_loss = 0.0;
+        e_loss = e_weight / inum * std::pow(etot_ml - etot_dft, 2);
+        atomicAdd(loss_ptr, e_loss);
+    }
+}
+
+
+template <typename CoordType>
+void find_loss_launcher(
+    CoordType *h_bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *h_binum,
+    int *h_bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType v_weight,
+    CoordType *h_betot_ml,
+    CoordType *h_betot_dft,
+    CoordType (*h_bforce_ml)[3],
+    CoordType (*h_bforce_dft)[3],
+    CoordType *h_bvirial_ml,
+    CoordType *h_bvirial_dft)
+{
+    int block_size_x = 64;
+    int grid_size_x = (batch_size*natoms_pad - 1) / block_size_x + 1;
+    dim3 grid_size(grid_size_x);
+    dim3 block_size(block_size_x);
+
+    CoordType *d_bloss_ptr;
+    int *d_binum;
+    int *d_bilist;
+    CoordType *d_betot_ml;
+    CoordType *d_betot_dft;
+    CoordType (*d_bforce_ml)[3];
+    CoordType (*d_bforce_dft)[3];
+    CoordType *d_bvirial_ml;
+    CoordType *d_bvirial_dft;
+
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bloss_ptr, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMemset(d_bloss_ptr, 0.0, sizeof(CoordType)*batch_size) );
+
+    CHECK_CUDA_API( cudaMalloc((void**)&d_binum, sizeof(int)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bilist, sizeof(int)*batch_size*natoms_pad) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_betot_ml, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_betot_dft, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bforce_ml, sizeof(CoordType)*batch_size*natoms_pad*3) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bforce_dft, sizeof(CoordType)*batch_size*natoms_pad*3) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bvirial_ml, sizeof(CoordType)*batch_size*9) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bvirial_dft, sizeof(CoordType)*batch_size*9) );
+
+    CHECK_CUDA_API( cudaMemcpy(d_binum, h_binum, sizeof(int)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bilist, h_bilist, sizeof(int)*batch_size*natoms_pad, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_betot_ml, h_betot_ml, sizeof(CoordType)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_betot_dft, h_betot_dft, sizeof(CoordType)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bforce_ml, h_bforce_dft, sizeof(CoordType)*batch_size*natoms_pad*3, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bforce_dft, h_bforce_ml, sizeof(CoordType)*batch_size*natoms_pad*3, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bvirial_ml, h_bvirial_ml, sizeof(CoordType)*batch_size*9, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bvirial_dft, h_bvirial_dft, sizeof(CoordType)*batch_size*9, cudaMemcpyHostToDevice) );
+
+    // Launch kernel function
+    find_loss_kernel<CoordType> KERNEL_ARG2(grid_size, block_size) (
+        d_bloss_ptr,
+        batch_size,
+        natoms_pad,
+        d_binum,
+        d_bilist,
+        e_weight,
+        f_weight,
+        v_weight,
+        d_betot_ml,
+        d_betot_dft,
+        d_bforce_ml,
+        d_bforce_dft,
+        d_bvirial_ml,
+        d_bvirial_dft);
+    CHECK_CUDA_API( cudaDeviceSynchronize() );
+    CHECK_CUDA_API( cudaGetLastError() );
+
+    CHECK_CUDA_API( cudaMemcpy(h_bloss_ptr, d_bloss_ptr, sizeof(CoordType)*batch_size, cudaMemcpyDeviceToHost) );
+
+    CHECK_CUDA_API( cudaFree(d_bloss_ptr) );
+    CHECK_CUDA_API( cudaFree(d_binum) );
+    CHECK_CUDA_API( cudaFree(d_bilist) );
+    CHECK_CUDA_API( cudaFree(d_betot_ml) );
+    CHECK_CUDA_API( cudaFree(d_betot_dft) );
+    CHECK_CUDA_API( cudaFree(d_bforce_ml) );
+    CHECK_CUDA_API( cudaFree(d_bforce_dft) );
+    CHECK_CUDA_API( cudaFree(d_bvirial_ml) );
+    CHECK_CUDA_API( cudaFree(d_bvirial_dft) );
+}
+
+
+template <typename CoordType>
+__global__
+void find_ef_loss_kernel(
+    CoordType *bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType *betot_ml,
+    CoordType *betot_dft,
+    CoordType (*bforce_ml)[3],
+    CoordType (*bforce_dft)[3])
+{
+    int nx = blockIdx.x * blockDim.x + threadIdx.x;
+    int istruct = nx / natoms_pad;
+    if (istruct >= batch_size)
+        return;
+    int ii = nx % natoms_pad;
+
+    CoordType *loss_ptr = &bloss_ptr[istruct];
+    CoordType etot_ml = betot_ml[istruct];
+    CoordType etot_dft = betot_dft[istruct];
+    CoordType (*force_ml)[3] = &bforce_ml[istruct*natoms_pad + 0];
+    CoordType (*force_dft)[3] = &bforce_dft[istruct*natoms_pad + 0];
+    int inum = binum[istruct];
+    int *ilist = &bilist[istruct*natoms_pad];
+
+    CoordType f_loss = 0.0;
+    if (ii < inum) {
+        int center_idx = ilist[ii];
+        for (int aa=0; aa<3; aa++)
+            f_loss += std::pow(force_ml[center_idx][aa] - force_dft[center_idx][aa], 2);
+        f_loss = f_weight / (3*inum) * f_loss;
+        atomicAdd(loss_ptr, f_loss);
+    }
+
+    if (nx == 0) {
+        CoordType e_loss = 0.0;
+        e_loss = e_weight / inum * std::pow(etot_ml - etot_dft, 2);
+        atomicAdd(loss_ptr, e_loss);
+    }
+}
+
+
+template <typename CoordType>
+void find_ef_loss_launcher(
+    CoordType *h_bloss_ptr,
+    int batch_size,
+    int natoms_pad,
+    int *h_binum,
+    int *h_bilist,
+    CoordType e_weight,
+    CoordType f_weight,
+    CoordType *h_betot_ml,
+    CoordType *h_betot_dft,
+    CoordType (*h_bforce_ml)[3],
+    CoordType (*h_bforce_dft)[3])
+{
+    int block_size_x = 64;
+    int grid_size_x = (batch_size*natoms_pad - 1) / block_size_x + 1;
+    dim3 grid_size(grid_size_x);
+    dim3 block_size(block_size_x);
+
+    CoordType *d_bloss_ptr;
+    int *d_binum;
+    int *d_bilist;
+    CoordType *d_betot_ml;
+    CoordType *d_betot_dft;
+    CoordType (*d_bforce_ml)[3];
+    CoordType (*d_bforce_dft)[3];
+    
+
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bloss_ptr, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMemset(d_bloss_ptr, 0.0, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_binum, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bilist, sizeof(int)*batch_size*natoms_pad) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_betot_ml, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_betot_dft, sizeof(CoordType)*batch_size) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bforce_ml, sizeof(CoordType)*batch_size*natoms_pad*3) );
+    CHECK_CUDA_API( cudaMalloc((void**)&d_bforce_dft, sizeof(CoordType)*batch_size*natoms_pad*3) );
+    
+    CHECK_CUDA_API( cudaMemcpy(d_binum, h_binum, sizeof(int)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bilist, h_bilist, sizeof(int)*batch_size*natoms_pad, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_betot_ml, h_betot_ml, sizeof(CoordType)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_betot_dft, h_betot_dft, sizeof(CoordType)*batch_size, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bforce_ml, h_bforce_ml, sizeof(CoordType)*batch_size*natoms_pad*3, cudaMemcpyHostToDevice) );
+    CHECK_CUDA_API( cudaMemcpy(d_bforce_dft, h_bforce_dft, sizeof(CoordType)*batch_size*natoms_pad*3, cudaMemcpyHostToDevice) );
+
+    // Launch kernel
+    find_ef_loss_kernel<CoordType> KERNEL_ARG2(grid_size, block_size) (
+        d_bloss_ptr,
+        batch_size,
+        natoms_pad,
+        d_binum,
+        d_bilist,
+        e_weight,
+        f_weight,
+        d_betot_ml,
+        d_betot_dft,
+        d_bforce_ml,
+        d_bforce_dft);
+
+    CHECK_CUDA_API( cudaDeviceSynchronize() );
+    CHECK_CUDA_API( cudaGetLastError());
+
+    CHECK_CUDA_API( cudaMemcpy(h_bloss_ptr, d_bloss_ptr, sizeof(CoordType)*batch_size, cudaMemcpyDeviceToHost) );
+    
+    CHECK_CUDA_API( cudaFree(d_bloss_ptr) );
+    CHECK_CUDA_API( cudaFree(d_binum) );
+    CHECK_CUDA_API( cudaFree(d_bilist) );
+    CHECK_CUDA_API( cudaFree(d_betot_ml) );
+    CHECK_CUDA_API( cudaFree(d_betot_dft) );
+    CHECK_CUDA_API( cudaFree(d_bforce_ml) );
+    CHECK_CUDA_API( cudaFree(d_bforce_dft) );
+}
+
+
+};  // namespace : nep
+};  // namespace : ai2pot
 
 #endif
---
+++ source/descriptor/nep/gpu/tests/test_nepLoss.cu
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+#include "../include/nep_utilities.cuh"
+#include "../include/nepLoss.cuh"
+
+
+class NepLossTest : public ::testing::Test
+{
+protected:
+    int batch_size;
+    int natoms_pad;
+
+    double *bloss;
+    int *binum;
+    int *bilist;
+    double e_weight;
+    double f_weight;
+    double v_weight;
+    double *betot_ml;
+    double *betot_dft;
+    double (*bforce_ml)[3];
+    double (*bforce_dft)[3];
+    double *bvirial_ml;
+    double *bvirial_dft;
+
+    static void SetUpTestSuite() {
+        std::cout << "NepLossTest (TestSuite) is setting up...\n";
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "NepLossTest (TestSuite) is tearing down...\n";
+    }
+
+    void SetUp() override {
+        batch_size = 1;
+        natoms_pad = 12;
+        
+        bloss = (double*)malloc(sizeof(double) * batch_size);
+        for (int ii=0; ii<batch_size; ii++)
+            bloss[ii] = 0.0;
+        binum = (int*)malloc(sizeof(double) * batch_size);
+        binum[0] = 12;
+        bilist = (int*)malloc(sizeof(int) * batch_size * natoms_pad);
+        for (int ii=0; ii<natoms_pad; ii++)
+            bilist[0*natoms_pad + ii] = ii;
+        e_weight = 1.0;
+        f_weight = 1.0;
+        v_weight = 1.0;
+        betot_ml = (double*)malloc(sizeof(double) * batch_size);
+        betot_ml[0] = 100;
+        betot_dft = (double*)malloc(sizeof(double) * batch_size);
+        betot_dft[0] = 102;
+
+        bforce_ml = (double (*)[3])malloc(sizeof(double) * batch_size * natoms_pad * 3);
+        bforce_dft = (double (*)[3])malloc(sizeof(double) * batch_size * natoms_pad * 3);
+        for (int ii=0; ii<natoms_pad; ii++) {
+            for (int aa=0; aa<3; aa++) {
+                bforce_ml[0*natoms_pad + ii][aa] = 1.00;
+                bforce_dft[0*natoms_pad + ii][aa] = 1.02 + ii*0.01;
+            }
+        }
+
+        bvirial_ml = (double*)malloc(sizeof(double) * batch_size * 9);
+        bvirial_dft = (double*)malloc(sizeof(double) * batch_size * 9);
+        for (int aa=0; aa<3; aa++) {
+            for (int bb=0; bb<3; bb++) {
+                bvirial_ml[0*9 + aa*3 + bb] = 1.00;
+                bvirial_dft[0*9 + aa*3 + bb] = 1.00 + (aa+bb)*0.01;
+            }
+        }
+    }
+
+    void TearDown() override {
+        free(bloss);
+        free(binum);
+        free(bilist);
+        free(betot_ml);
+        free(betot_dft);
+        free(bforce_ml);
+        free(bforce_dft);
+        free(bvirial_ml);
+        free(bvirial_dft);
+    }
+};  // class : NepLossTest
+
+
+TEST_F(NepLossTest, find_loss) {
+    ai2pot::nep::find_loss_launcher<double>(
+        bloss,
+        batch_size,
+        natoms_pad,
+        binum,
+        bilist,
+        e_weight,
+        f_weight,
+        v_weight,
+        betot_ml,
+        betot_dft,
+        bforce_ml,
+        bforce_dft,
+        bvirial_ml,
+        bvirial_dft);
+
+printf("1. Loss = %.15f\n", bloss[0]);
+}
+
+
+TEST_F(NepLossTest, find_ef_loss) {
+    ai2pot::nep::find_ef_loss_launcher<double>(
+        bloss,
+        batch_size,
+        natoms_pad,
+        binum,
+        bilist,
+        e_weight,
+        f_weight,
+        betot_ml,
+        betot_dft,
+        bforce_ml,
+        bforce_dft);
+printf("1. Loss = %.15f\n", bloss[0]);
+}
+
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
