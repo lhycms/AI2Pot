Fix bug: gradient of Blm(4, 0) for GPU

test/models/nep/test_lit_nep.py
@@ -13,6 +13,9 @@ from ai2pot.utils.prepot import ExtxyzShifter
 from ai2pot.models.nep.nep_train_utils import NepDescriptorNormCallback
 
 
+torch.manual_seed(2143)
+torch.set_num_threads(16)
+
 
 TEST_FILES_DIR = os.path.join(os.getenv("AI2POT_PATH"), "test", "test_data")
 PbTe_EXTXYZ_PATH = "/data/home/liuhanyu/mycode/AI2Pot-Tutorials/data/XYZ/Li_battery/train.xyz"#os.path.join(TEST_FILES_DIR, "XYZ", "11_NEP_potential_PbTe", "train_m.xyz")
@@ -39,7 +42,7 @@ class LitNepTest(unittest.TestCase):
         num_neurons: int = 30
 
 
-        max_epochs: int = 200
+        max_epochs: int = 500
         # Lr hyperparameters
         lr_start: float = 1e-2
         lr_end: float = 5e-4

test/models/nep/test_nep_utils.py
@@ -34,10 +34,10 @@ torch.set_num_threads(16)
 class Nep4ExtxyzTest(unittest.TestCase):
     def setUp(self):
         print("Nep4ExtxyzTest (TestCase) is setting up...")
-        self.checkpoint_path: str = "/data/home/liuhanyu/mycode/AI2Pot/lightning_logs/version_0/checkpoints/epoch=199-step=5000.ckpt"
+        self.checkpoint_path: str = "/data/home/liuhanyu/mycode/AI2Pot/lightning_logs/version_1/checkpoints/epoch=499-step=12500.ckpt"
         self.testset_path: str = EXTXYZ_PATH
-        self.map_location: str = "cpu"        
-        self.torch_float_dtype: torch._C.dtype = torch.float64
+        self.map_location: str = "cuda"        
+        self.torch_float_dtype: torch._C.dtype = torch.float32
         self.nep_extxyz: Nep4Extxyz = Nep4Extxyz(checkpoint_path=self.checkpoint_path,
                                                  testset_path=self.testset_path,
                                                  map_location=self.map_location,