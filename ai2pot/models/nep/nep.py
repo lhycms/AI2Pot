Fix bug for RMSE calculation

ai2pot/models/nep/nep.py
@@ -157,7 +157,7 @@ class Nep(nn.Module):
                         brcs_tensor: torch.Tensor,
                         btypes_tensor: torch.Tensor,
                         nghost: int):
-        bmse_tensor: torch.Tensor = nepToEFLossOp(
+        bmse_tensor, e_rmse_tensor, f_rmse_tensor = nepToEFLossOp(
             e_weight,
             f_weight,
             betot_dft_tensor,
@@ -185,9 +185,12 @@ class Nep(nn.Module):
             self.zbl_rmax,
             self.zbl_rmin,
             self.zbl_cks_tensor,
-            self.zbl_dks_tensor)[0]
+            self.zbl_dks_tensor)
+        bmse_tensor: torch.Tensor
+        e_rmse_tensor: torch.Tensor
+        f_rmse_tensor: torch.Tensor
 
-        return bmse_tensor
+        return bmse_tensor, e_rmse_tensor, f_rmse_tensor
 
 
     def predict_ef(self,

test/models/nep/test_nep_utils.py
@@ -24,7 +24,7 @@ EXTXYZ_PATH: str = os.path.join(TEST_FILES_DIR,
                                 "XYZ",
                                 "11_NEP_potential_PbTe",
                                 "train_m.xyz")
-EXTXYZ_PATH = "/data/home/liuhanyu/mycode/AI2Pot-Tutorials/data/XYZ/Li_battery/train.xyz"
+#EXTXYZ_PATH = "/data/home/liuhanyu/mycode/AI2Pot-Tutorials/data/XYZ/Li_battery/train.xyz"
 
 torch.set_num_threads(16)
 
@@ -34,7 +34,7 @@ torch.set_num_threads(16)
 class Nep4ExtxyzTest(unittest.TestCase):
     def setUp(self):
         print("Nep4ExtxyzTest (TestCase) is setting up...")
-        self.checkpoint_path: str = "/data/home/liuhanyu/mycode/AI2Pot/lightning_logs/version_1/checkpoints/epoch=499-step=12500.ckpt"
+        self.checkpoint_path: str = "/data/home/liuhanyu/mycode/AI2Pot/lightning_logs/version_5/checkpoints/epoch=199-step=5000.ckpt"
         self.testset_path: str = EXTXYZ_PATH
         self.map_location: str = "cuda"        
         self.torch_float_dtype: torch._C.dtype = torch.float32