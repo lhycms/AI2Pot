47ca5318c028f1b3f93e50ea4d1613ac909e2052
lhycms <1404293933@qq.com>
2026-02-15 09:22:47 +0800

    Add LinearMtp CPU launcher

---
+++ source/descriptor/mtpr/include/linearMtp_cpu_launcher.h
@@ -0,0 +1,343 @@
+/*
+    Copyright 2025 Hanyu Liu
+    This file is part of AI2Pot.
+    AI2Pot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    AI2Pot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with AI2Pot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef AI2POT_MTPR_LINEAR_MTP_CPU_LAUNCHER_H
+#define AI2POT_MTPR_LINEAR_MTP_CPU_LAUNCHER_H
+
+
+#include "./linearMtp.h"
+
+
+namespace ai2pot {
+namespace mtpr {
+
+
+template <typename CoordType>
+static void find_efv_cpu_launcher(
+    CoordType *betot,
+    CoordType (*bforce)[3],
+    CoordType *bvirial,
+    int chebyshev_size,
+    CoordType *coeffs,
+    CoordType *linear_coeffs,
+    CoordType *type_bias,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin,
+    CoordType *q_scaler);
+
+template <typename CoordType>
+static void find_ef_cpu_launcher(
+    CoordType *betot,
+    CoordType (*bforce)[3],
+    int chebyshev_size,
+    CoordType *coeffs,
+    CoordType *linear_coeffs,
+    CoordType *type_bias,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin,
+    CoordType *q_scaler);
+
+template <typename CoordType>
+static void find_descriptors_cpu_launcher(
+    CoordType *bdescriptors,
+    int chebyshev_size,
+    CoordType *coeffs,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin);
+
+
+
+
+
+template <typename CoordType>
+void find_efv_cpu_launcher(
+    CoordType *betot,
+    CoordType (*bforce)[3],
+    CoordType *bvirial,
+    int chebyshev_size,
+    CoordType *coeffs,
+    CoordType *linear_coeffs,
+    CoordType *type_bias,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin,
+    CoordType *q_scaler)
+{
+    for (int bb=0; bb<batch_size; bb++) {
+        CoordType *etot_ptr = betot + bb;
+        CoordType (*force)[3] = &bforce[bb*(natoms_pad + nghost)];
+        CoordType *virial = &bvirial[bb*9];
+        int inum = binum[bb];
+        int *ilist = &bilist[bb*natoms_pad];
+        int *numneigh = &bnumneigh[bb*natoms_pad];
+        int *firstneigh = &bfirstneigh[bb*natoms_pad*umax_num_neigh_atoms];
+        CoordType (*rcs)[3] = &brcs[bb*natoms_pad*umax_num_neigh_atoms];
+        int *types = &btypes[bb*(natoms_pad+nghost)];
+
+        LinearMtp<CoordType>::find_efv(
+            *etot_ptr,
+            force,
+            virial,
+            chebyshev_size,
+            coeffs,
+            linear_coeffs,
+            type_bias,
+            alpha_moments_count,
+            alpha_index_basic_count,
+            alpha_index_basic,
+            alpha_index_times_count,
+            alpha_index_times,
+            alpha_scalar_moments,
+            alpha_moment_mapping,
+            nmus,
+            inum,
+            ilist,
+            numneigh,
+            firstneigh,
+            rcs,
+            types,
+            ntypes,
+            type_map,
+            umax_num_neigh_atoms,
+            nghost,
+            rmax,
+            rmin,
+            q_scaler);
+    }
+}
+
+
+template <typename CoordType>
+void find_ef_cpu_launcher(
+    CoordType *betot,
+    CoordType (*bforce)[3],
+    int chebyshev_size,
+    CoordType *coeffs,
+    CoordType *linear_coeffs,
+    CoordType *type_bias,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin,
+    CoordType *q_scaler)
+{
+    for (int bb=0; bb<batch_size; bb++) {
+        CoordType *etot_ptr = betot + bb;
+        CoordType (*force)[3] = &bforce[bb*(natoms_pad + nghost)];
+        int inum = binum[bb];
+        int *ilist = &bilist[bb*natoms_pad];
+        int *numneigh = &bnumneigh[bb*natoms_pad];
+        int *firstneigh = &bfirstneigh[bb*natoms_pad*umax_num_neigh_atoms];
+        CoordType (*rcs)[3] = &brcs[bb*natoms_pad*umax_num_neigh_atoms];
+        int *types = &btypes[bb*(natoms_pad+nghost)];
+
+        LinearMtp<CoordType>::find_ef(
+            *etot_ptr,
+            force,
+            chebyshev_size,
+            coeffs,
+            linear_coeffs,
+            type_bias,
+            alpha_moments_count,
+            alpha_index_basic_count,
+            alpha_index_basic,
+            alpha_index_times_count,
+            alpha_index_times,
+            alpha_scalar_moments,
+            alpha_moment_mapping,
+            nmus,
+            inum,
+            ilist,
+            numneigh,
+            firstneigh,
+            rcs,
+            types,
+            ntypes,
+            type_map,
+            umax_num_neigh_atoms,
+            nghost,
+            rmax,
+            rmin,
+            q_scaler);
+    }
+}
+
+template <typename CoordType>
+void find_descriptors_cpu_launcher(
+    CoordType *bdescriptors,
+    int chebyshev_size,
+    CoordType *coeffs,
+    const int alpha_moments_count,
+    const int alpha_index_basic_count,
+    const int (*alpha_index_basic)[4],
+    const int alpha_index_times_count,
+    const int (*alpha_index_times)[4],
+    const int alpha_scalar_moments,
+    const int *alpha_moment_mapping,
+    int nmus,
+    int batch_size,
+    int natoms_pad,
+    int *binum,
+    int *bilist,
+    int *bnumneigh,
+    int *bfirstneigh,
+    CoordType (*brcs)[3],
+    int *btypes,
+    int ntypes,
+    int *type_map,
+    int umax_num_neigh_atoms,
+    int nghost,
+    CoordType rmax,
+    CoordType rmin)
+{
+    for (int bb=0; bb<batch_size; bb++) {
+        CoordType *descriptors = &bdescriptors[bb*natoms_pad*alpha_scalar_moments];
+        int inum = binum[bb];
+        int *ilist = &bilist[bb*natoms_pad];
+        int *numneigh = &bnumneigh[bb*natoms_pad];
+        int *firstneigh = &bfirstneigh[bb*natoms_pad*umax_num_neigh_atoms];
+        CoordType (*rcs)[3] = &brcs[bb*natoms_pad*umax_num_neigh_atoms];
+        int *types = &btypes[bb*natoms_pad];
+
+        LinearMtp<CoordType>::find_descriptors(
+            descriptors,
+            chebyshev_size,
+            coeffs,
+            alpha_moments_count,
+            alpha_index_basic_count,
+            alpha_index_basic,
+            alpha_index_times_count,
+            alpha_index_times,
+            alpha_scalar_moments,
+            alpha_moment_mapping,
+            nmus,
+            inum,
+            ilist,
+            numneigh,
+            firstneigh,
+            rcs,
+            types,
+            ntypes,
+            type_map,
+            umax_num_neigh_atoms,
+            nghost,
+            rmax,
+            rmin);
+    }
+}
+
+
+};  // namespace : mtpr
+};  // namespace : ai2pot
+
+
+#endif
---
+++ source/descriptor/mtpr/tests/test_linearMtp_cpu_launcher.cc
@@ -0,0 +1,418 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+
+#include "../../../nblist/include/structure.h"
+#include "../../../nblist/include/neighborList.h"
+#include "../include/mtpParam.h"
+#include "../include/linearMtp.h"
+#include "../include/linearMtp_cpu_launcher.h"
+
+
+typedef double real;
+
+
+class LinearMtpCPULauncher : public :: testing::Test
+{
+protected:
+    int ntypes;
+    std::vector<std::string> filenames;
+    ai2pot::mtpr::MtpParam mtp_param;
+    int chebyshev_size;
+    real *coeffs;
+    real *linear_coeffs;
+    real *type_bias;
+    real rmax;
+    real rmin;
+
+    int num_atoms;
+    real basis_vectors[3][3];
+    int atomic_numbers[12];
+    real frac_coords[12][3];
+    real rcut;
+    real bin_size_xyz[3];
+    bool pbc_xyz[3];
+    int type_map[2];
+
+    int batch_size;
+    int natoms_pad;
+    int umax_num_neigh_atoms;
+    int *binum;
+    int *bilist;
+    int *bnumneigh;
+    int *bfirstneigh;
+    real *brcs;
+    int *btypes;
+    int nghost;
+
+    ai2pot::Structure<real> structure;
+    ai2pot::NeighborList<real> neighbor_list;
+
+
+    real *betot;
+    real *betot_;
+    real (*bforce)[3];
+    real (*bforce_)[3];
+    real *bvirial;
+    real *bvirial_;
+
+    // Loss derivatives
+    real *bloss_der2coeffs;
+    real *bloss_der2linear_coeffs;
+    real *bloss_der2type_bias;
+
+    // Loss
+    real e_weight;
+    real f_weight;
+    real v_weight;
+    real *betot_dft;
+    real (*bforce_dft)[3];
+    real *bvirial_dft;
+
+    // Descriptors
+    real *bdescriptors;
+
+    // q factors
+    real *q_scaler;
+
+    static void SetUpTestSuite() {
+        std::cout << "LinearMtpCPULauncher (TestSuite) is setting up...\n";
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "LinearMtpCPULauncher (TestSuite) is tearing down...\n";
+    }
+
+    void SetUp() override {
+        ntypes = 2;
+        filenames = {
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/depreciated-02.almtp", 
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/depreciated-04.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/06.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/08.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/10.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/12.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/14.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/16.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/18.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/20.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/22.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/24.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/26.almtp",
+            (std::string)std::getenv("AI2POT_PATH") + "/source/descriptor/mtpr/MTP_templates/28.almtp"
+        };
+        mtp_param._load(filenames[7]);
+        chebyshev_size = 8;
+        rmax = 5.0;
+        rmin = 2.0;
+        coeffs = (real*)malloc(sizeof(real) * ntypes * ntypes * mtp_param.nmus() * chebyshev_size);
+        linear_coeffs = (real*)malloc(sizeof(real) * mtp_param.alpha_scalar_moments());
+        type_bias = (real*)malloc(sizeof(real) * ntypes);
+        for (int ii=0; ii<ntypes*ntypes*mtp_param.nmus()*chebyshev_size; ii++)
+            coeffs[ii] = 0.01 + 0.001 * ii;
+        for (int ii=0; ii<mtp_param.alpha_scalar_moments(); ii++)
+            linear_coeffs[ii] = 0.01 + 0.001 * ii;
+        type_bias[0] = -0.1;
+        type_bias[1] = -0.2;
+
+
+        // Establish neighbor list
+        num_atoms = 12;
+        basis_vectors[0][0] = 3.1903157348;
+        basis_vectors[0][1] = 5.5257885468;
+        basis_vectors[0][2] = 0.0000000000;
+        basis_vectors[1][0] = -6.3806307800;
+        basis_vectors[1][1] = 0.0000000000;
+        basis_vectors[1][2] = 0.0000000000;
+        basis_vectors[2][0] = 0.0000000000;
+        basis_vectors[2][1] = 0.0000000000;
+        basis_vectors[2][2] = 23.1297687334;
+
+        // 42: 0;  16: 1
+        atomic_numbers[0] = 0;
+        atomic_numbers[1] = 1;
+        atomic_numbers[2] = 1;
+        atomic_numbers[3] = 0;
+        atomic_numbers[4] = 1;
+        atomic_numbers[5] = 1;
+        atomic_numbers[6] = 0;
+        atomic_numbers[7] = 1;
+        atomic_numbers[8] = 1;
+        atomic_numbers[9] = 0; 
+        atomic_numbers[10] = 1;
+        atomic_numbers[11] = 1;
+
+        frac_coords[0][0] = 0.333333333333;
+        frac_coords[0][1] = 0.166666666667;
+        frac_coords[0][2] = 0.500000000000;
+        frac_coords[1][0] = 0.166666666667;
+        frac_coords[1][1] = 0.333333333333;
+        frac_coords[1][2] = 0.432343276548;
+        frac_coords[2][0] = 0.166666666667;
+        frac_coords[2][1] = 0.333333333333;
+        frac_coords[2][2] = 0.567656723452;
+        frac_coords[3][0] = 0.333333333333;
+        frac_coords[3][1] = 0.666666666667;
+        frac_coords[3][2] = 0.500000000000;
+        frac_coords[4][0] = 0.166666666667;
+        frac_coords[4][1] = 0.833333333333;
+        frac_coords[4][2] = 0.432343276548;
+        frac_coords[5][0] = 0.166666666667;
+        frac_coords[5][1] = 0.833333333333;
+        frac_coords[5][2] = 0.567656723452;
+        frac_coords[6][0] = 0.833333333333;
+        frac_coords[6][1] = 0.166666666667;
+        frac_coords[6][2] = 0.500000000000;
+        frac_coords[7][0] = 0.666666666667;
+        frac_coords[7][1] = 0.333333333333;
+        frac_coords[7][2] = 0.432343276548;
+        frac_coords[8][0] = 0.666666666667;
+        frac_coords[8][1] = 0.333333333333;
+        frac_coords[8][2] = 0.567656723452;
+        frac_coords[9][0] = 0.833333333333;
+        frac_coords[9][1] = 0.666666666667;
+        frac_coords[9][2] = 0.500000000000;
+        frac_coords[10][0] = 0.666666666667;
+        frac_coords[10][1] = 0.833333333333;
+        frac_coords[10][2] = 0.432343276548;
+        frac_coords[11][0] = 0.666666666667;
+        frac_coords[11][1] = 0.833333333333;
+        frac_coords[11][2] = 0.567656723452;
+
+        rcut = 5.0;
+        bin_size_xyz[0] = 2.5;
+        bin_size_xyz[1] = 2.5;
+        bin_size_xyz[2] = 2.5;
+        pbc_xyz[0] = true;
+        pbc_xyz[1] = true;
+        pbc_xyz[2] = true;
+
+        type_map[0] = 42;
+        type_map[1] = 16;
+
+        structure = ai2pot::Structure<real>(num_atoms, basis_vectors, atomic_numbers, frac_coords, false);
+        neighbor_list = ai2pot::NeighborList<real>(structure, rcut, bin_size_xyz, pbc_xyz, true);
+        batch_size = 1;
+        natoms_pad = 12;
+        umax_num_neigh_atoms = 20;
+        nghost = 0;
+        binum = (int*)malloc(sizeof(int) * batch_size);
+        bilist = (int*)malloc(sizeof(int) * batch_size * natoms_pad);
+        bnumneigh = (int*)malloc(sizeof(int) * batch_size * natoms_pad);
+        bfirstneigh = (int*)malloc(sizeof(int) * batch_size * natoms_pad * umax_num_neigh_atoms);
+        brcs = (real*)malloc(sizeof(real) * batch_size * natoms_pad * umax_num_neigh_atoms * 3);
+        btypes = (int*)malloc(sizeof(int) * batch_size * (natoms_pad + nghost));
+        for (int bb=0; bb<batch_size; bb++)
+            neighbor_list.find_info4mlff(
+                binum[bb],
+                &bilist[bb*natoms_pad],
+                &bnumneigh[bb*natoms_pad],
+                &bfirstneigh[bb*natoms_pad*umax_num_neigh_atoms],
+                &brcs[bb*natoms_pad*umax_num_neigh_atoms*3],
+                &btypes[bb*(natoms_pad+nghost)],
+                nghost,
+                umax_num_neigh_atoms);
+
+        betot = (real *)malloc(sizeof(real) * batch_size);
+        memset(betot, 0.0, sizeof(real)*batch_size);
+        bforce = (real (*)[3])malloc(sizeof(real) * batch_size * (natoms_pad+nghost) * 3);
+        memset(bforce, 0.0, sizeof(real)*batch_size*(natoms_pad+nghost)*3);
+        bvirial = (real*)malloc(sizeof(real) * batch_size * 9);
+        memset(bvirial, 0.0, sizeof(real)*batch_size*9);
+
+        betot_ = (real *)malloc(sizeof(real) * batch_size);
+        memset(betot_, 0.0, batch_size);
+        bforce_ = (real (*)[3])malloc(sizeof(real) * batch_size * (natoms_pad+nghost) * 3);
+        memset(bforce_, 0.0, sizeof(real)*batch_size*(natoms_pad+nghost)*3);
+        bvirial_ = (real*)malloc(sizeof(real) * batch_size * 9);
+        memset(bvirial_, 0.0, sizeof(real) * batch_size * 9);
+
+        // Loss derivative
+        bloss_der2coeffs = (real*)malloc(sizeof(real) * batch_size * ntypes * ntypes * mtp_param.nmus() * chebyshev_size);
+        memset(bloss_der2coeffs, 0, sizeof(real) * batch_size * ntypes * ntypes * mtp_param.nmus() * chebyshev_size);
+        bloss_der2linear_coeffs = (real*)malloc(sizeof(real) * batch_size * mtp_param.alpha_scalar_moments());
+        memset(bloss_der2linear_coeffs, 0, sizeof(real) * batch_size * mtp_param.alpha_scalar_moments());
+        bloss_der2type_bias = (real*)malloc(sizeof(real) * batch_size * ntypes);
+        memset(bloss_der2type_bias, 0, sizeof(real) * batch_size * ntypes);
+
+        // Loss
+        e_weight = 0.1;
+        f_weight = 0.2;
+        v_weight = 0.3;
+        betot_dft = (real*)malloc(sizeof(real) * batch_size);
+        memset(betot_dft, 0, sizeof(real)*batch_size);
+        bforce_dft = (real (*)[3])malloc(sizeof(real) * batch_size * natoms_pad * 3);
+        memset(bforce_dft, 0, sizeof(real)*batch_size*natoms_pad*3);
+        bvirial_dft = (real*)malloc(sizeof(real) * batch_size * 9);
+        memset(bvirial_dft, 0, sizeof(real)*batch_size*9);
+
+        // Descriptors
+        bdescriptors = (real*)malloc(sizeof(real) * batch_size * natoms_pad * mtp_param.alpha_scalar_moments());
+        memset(bdescriptors, 0, sizeof(real) * batch_size * natoms_pad * mtp_param.alpha_scalar_moments());
+
+        // 
+        q_scaler = (real*)malloc(sizeof(real) * mtp_param.alpha_scalar_moments());
+        for (int ii=0; ii<mtp_param.alpha_scalar_moments(); ii++) {
+            q_scaler[ii] = 0.67 + 0.05 * ii;
+        }
+    }
+
+    void TearDown() override {
+        free(coeffs);
+        free(linear_coeffs);
+        free(type_bias);
+
+        free(binum);
+        free(bilist);
+        free(bnumneigh);
+        free(bfirstneigh);
+        free(brcs);
+        free(btypes);
+
+        free(betot);
+        free(bforce);
+        free(bvirial);
+        free(betot_);
+        free(bforce_);
+        free(bvirial_);
+
+        free(bloss_der2coeffs);
+        free(bloss_der2linear_coeffs);
+        free(bloss_der2type_bias);
+        free(betot_dft);
+        free(bforce_dft);
+        free(bvirial_dft);
+
+        free(bdescriptors);
+        free(q_scaler);
+    }
+};  // class : LinearMtpCPULauncher
+
+
+TEST_F(LinearMtpCPULauncher, find_efv_cpu_launcher) {
+    int center_idx_modify = 1;
+    int direction1_idx_modify = 2;
+
+    ai2pot::mtpr::find_efv_cpu_launcher<real>(
+        betot,
+        bforce,
+        bvirial,
+        chebyshev_size,
+        coeffs,
+        linear_coeffs,
+        type_bias,
+        mtp_param.alpha_moments_count(),
+        mtp_param.alpha_index_basic_count(),
+        mtp_param.alpha_index_basic(),
+        mtp_param.alpha_index_times_count(),
+        mtp_param.alpha_index_times(),
+        mtp_param.alpha_scalar_moments(),
+        mtp_param.alpha_moment_mapping(),
+        mtp_param.nmus(),
+        batch_size,
+        natoms_pad,
+        binum,
+        bilist,
+        bnumneigh,
+        bfirstneigh,
+        (real (*)[3])brcs,
+        btypes,
+        ntypes,
+        type_map,
+        umax_num_neigh_atoms,
+        nghost,
+        rmax,
+        rmin,
+        q_scaler);
+
+printf("1.1. energy = %.15lf\n", betot[0]);
+printf("1.2. force[%d][%d] calculated by custom code = %.15lf\n", center_idx_modify, direction1_idx_modify, bforce[center_idx_modify][direction1_idx_modify]);
+printf("1.3. force=\n");
+for (int ii=0; ii<natoms_pad; ii++)
+    printf("\t\t%3d: [%.15f, %.15f, %.15f]\n", ii, bforce[ii][0], bforce[ii][1], bforce[ii][2]);
+}
+
+
+TEST_F(LinearMtpCPULauncher, find_ef_cpu_launcher) {
+    int center_idx_modify = 1;
+    int direction1_idx_modify = 2;
+    
+    ai2pot::mtpr::find_ef_cpu_launcher<real>(
+        betot,
+        bforce,
+        chebyshev_size,
+        coeffs,
+        linear_coeffs,
+        type_bias,
+        mtp_param.alpha_moments_count(),
+        mtp_param.alpha_index_basic_count(),
+        mtp_param.alpha_index_basic(),
+        mtp_param.alpha_index_times_count(),
+        mtp_param.alpha_index_times(),
+        mtp_param.alpha_scalar_moments(),
+        mtp_param.alpha_moment_mapping(),
+        mtp_param.nmus(),
+        batch_size,
+        natoms_pad,
+        binum,
+        bilist,
+        bnumneigh,
+        bfirstneigh,
+        (real (*)[3])brcs,
+        btypes,
+        ntypes,
+        type_map,
+        umax_num_neigh_atoms,
+        nghost,
+        rmax,
+        rmin,
+        q_scaler);
+    
+printf("1.1. energy = %.15lf\n", betot[0]);
+printf("1.2. force[%d][%d] calculated by custom code = %.15lf\n", center_idx_modify, direction1_idx_modify, bforce[center_idx_modify][direction1_idx_modify]);
+printf("1.3. force=\n");
+for (int ii=0; ii<natoms_pad; ii++)
+    printf("\t\t%3d: [%.15f, %.15f, %.15f]\n", ii, bforce[ii][0], bforce[ii][1], bforce[ii][2]);
+}
+
+
+TEST_F(LinearMtpCPULauncher, find_descriptors_cpu_launcher) {
+    int center_idx_modify = 1;
+    int direction1_idx_modify = 2;
+    
+    ai2pot::mtpr::find_descriptors_cpu_launcher<real>(
+        bdescriptors,
+        chebyshev_size,
+        coeffs,
+        mtp_param.alpha_moments_count(),
+        mtp_param.alpha_index_basic_count(),
+        mtp_param.alpha_index_basic(),
+        mtp_param.alpha_index_times_count(),
+        mtp_param.alpha_index_times(),
+        mtp_param.alpha_scalar_moments(),
+        mtp_param.alpha_moment_mapping(),
+        mtp_param.nmus(),
+        batch_size,
+        natoms_pad,
+        binum,
+        bilist,
+        bnumneigh,
+        bfirstneigh,
+        (real (*)[3])brcs,
+        btypes,
+        ntypes,
+        type_map,
+        umax_num_neigh_atoms,
+        nghost,
+        rmax,
+        rmin);
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
