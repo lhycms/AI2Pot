28f7ee6e55cce33a1a773ce45ba1b2300b3eeb92
lhycms <1404293933@qq.com>
2026-06-13 09:49:56 +0800

    Remove chgnet plugin

--- ai2pot/chgnet/__init__.py
+++
@@ -1,7 +0,0 @@
-from chgnet.utils import parse_vasp_dir
-from chgnet.utils import read_json
-from chgnet.data.dataset import StructureData, get_train_val_test_loader
-from chgnet.model import CHGNet
-from chgnet.trainer import trainer, Trainer
-from chgnet.model.dynamics import MolecularDynamics
-from chgnet.data.dataset import StructureData, get_train_val_test_loader
